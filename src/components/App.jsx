1758630be7bda0e970d4eefe562864f30655436e
ChristianEdwardPadilla <christianepadilla@gmail.com>
2019-04-18 17:41:34 -0400

    base electron config
    Co-authored-by: Tolga Mizrakci <tolgamizrakci@gmail.com>
    Co-authored-by: Adam Singer <spincycle01@yahoo.com>
    Co-authored-by: Shlomo Porges <porges.s@gmail.com>

---
+++ src/components/App.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import '../public/styles/style.css';
+import { MuiThemeProvider } from '@material-ui/core/styles';
+import theme from './theme';
+import AppContainer from '../containers/AppContainer.jsx';
+
+class App extends Component {
+  render() {
+    return (
+      <MuiThemeProvider theme={theme}>
+        <div className="app">
+          <div>
+            <header style={{ height: '40px', width: '100%' }}>ReacType</header>
+            <AppContainer />
+          </div>
+        </div>
+      </MuiThemeProvider>
+    );
+  }
+}
+
+export default App;
