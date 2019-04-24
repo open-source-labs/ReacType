d2446e1e65cdbc4686bf1d201cd0426e54f74763
ChristianEdwardPadilla <christianepadilla@gmail.com>
2019-04-24 09:30:47 -0400

    added local state to KonvaStage to keep track of size of inner column, scroll bars no longer show up

--- src/components/KonvaStage.jsx
+++ src/components/KonvaStage.jsx
@@ -7,16 +7,40 @@ import TransformerComponent from './TransformerComponent.jsx';
 import Rectangle from './Rectangle.jsx';
 
 class KonvaStage extends Component {
-  state = {
-    x: undefined,
-    y: undefined,
-  };
-
   constructor(props) {
     super(props);
+    this.state = {
+      x: undefined,
+      y: undefined,
+      stageWidth: 1000,
+      stageHeight: 1000,
+    };
     this.main = createRef();
     this.group = createRef();
   }
+
+  componentDidMount() {
+    this.checkSize();
+    // here we should add listener for "container" resize
+    // take a look here https://developers.google.com/web/updates/2016/10/resizeobserver
+    // for simplicity I will just listen window resize
+    window.addEventListener('resize', this.checkSize);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.checkSize);
+  }
+
+  checkSize = () => {
+    const width = this.container.offsetWidth;
+    const height = this.container.offsetHeight;
+    console.log('setting width: ', this.state.stageWidth, width);
+    console.log('setting height: ', this.state.heightWidth, height);
+    this.setState({
+      stageWidth: width,
+      stageHeight: height,
+    });
+  };
 
   handleStageMouseDown = (e) => {
     // // clicked on stage - clear selection
@@ -37,10 +61,6 @@ class KonvaStage extends Component {
     this.props.changeFocusChild({ childId: rectChildId });
   };
 
-  componentDidMount() {
-    // this.props.setImage();
-  }
-
   render() {
     const {
       components,
@@ -56,39 +76,50 @@ class KonvaStage extends Component {
     const { selectedShapeName } = this.state;
 
     return (
-      <Stage
-        // ref={(node) => {
-        //   this.stage = node;
-        // }}
-        onMouseDown={this.handleStageMouseDown}
-        width={window.innerWidth / 2}
-        height={window.innerHeight}
+      <div
+        style={{
+          width: '100%',
+          height: '100%',
+          // border: '1px solid grey',
+        }}
+        ref={(node) => {
+          this.container = node;
+        }}
       >
-        <Layer>
-          {components
-            .find(comp => comp.title === focusComponent.title)
-            .childrenArray.map((child, i) => (
-              <Rectangle
-                key={`${i}${child.componentName}`}
-                components={components}
-                componentId={focusComponent.id}
-                componentName={child.componentName}
-                focusChild={focusChild}
-                childId={child.childId}
-                x={child.position.x}
-                y={child.position.y}
-                scaleX={1}
-                scaleY={1}
-                width={child.position.width}
-                height={child.position.height}
-                title={child.componentName + child.childId}
-                color={child.color}
-                handleTransform={handleTransform}
-                draggable={true}
-              />
-            ))}
-        </Layer>
-      </Stage>
+        <Stage
+          ref={(node) => {
+            this.stage = node;
+          }}
+          onMouseDown={this.handleStageMouseDown}
+          width={this.state.stageWidth}
+          height={this.state.stageHeight - 10}
+        >
+          <Layer>
+            {components
+              .find(comp => comp.title === focusComponent.title)
+              .childrenArray.map((child, i) => (
+                <Rectangle
+                  key={`${i}${child.componentName}`}
+                  components={components}
+                  componentId={focusComponent.id}
+                  componentName={child.componentName}
+                  focusChild={focusChild}
+                  childId={child.childId}
+                  x={child.position.x}
+                  y={child.position.y}
+                  scaleX={1}
+                  scaleY={1}
+                  width={child.position.width}
+                  height={child.position.height}
+                  title={child.componentName + child.childId}
+                  color={child.color}
+                  handleTransform={handleTransform}
+                  draggable={true}
+                />
+              ))}
+          </Layer>
+        </Stage>
+      </div>
     );
   }
 }
