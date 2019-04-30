9b6a3b1bf9d8869d62fa8500df5794da78773940
Tolga Mizrakci <tolgamizrakci@Tolgas-MacBook-Pro.local>
2019-04-30 14:24:56 -0400

    adding htmlcomp panel

--- src/containers/LeftContainer.jsx
+++ src/containers/LeftContainer.jsx
@@ -1,34 +1,71 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { compose } from "redux";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
 // import PropTypes from 'prop-types';
-import FormControl from "@material-ui/core/FormControl";
-import TextField from "@material-ui/core/TextField";
-import Button from "@material-ui/core/Button";
-import AddIcon from "@material-ui/icons/Add";
-import Grid from "@material-ui/core/Grid";
-import { withStyles } from "@material-ui/core/styles";
-import LeftColExpansionPanel from "../components/LeftColExpansionPanel.jsx";
+import FormControl from '@material-ui/core/FormControl';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import AddIcon from '@material-ui/icons/Add';
+import Grid from '@material-ui/core/Grid';
+import { withStyles } from '@material-ui/core/styles';
+import LeftColExpansionPanel from '../components/LeftColExpansionPanel.jsx';
+import HTMLComponentPanel from '../components/HTMLComponentPanel.jsx';
+
 // import createModal from '../utils/createModal.util';
-import * as actions from "../actions/components";
+import * as actions from '../actions/components';
+
+function styles() {
+  return {
+    cssLabel: {
+      color: 'white',
+
+      '&$cssFocused': {
+        color: 'green',
+      },
+    },
+    cssFocused: {},
+    input: {
+      color: '#fff',
+      opacity: '0.7',
+      marginBottom: '10px',
+    },
+    underline: {
+      color: 'white',
+      '&::before': {
+        color: 'white',
+      },
+    },
+    button: {
+      color: '#fff',
+
+      '&:disabled': {
+        color: 'grey',
+      },
+    },
+    clearButton: {
+      top: '96%',
+      position: 'sticky!important',
+      zIndex: '1',
+
+      '&:disabled': {
+        color: 'grey',
+        backgroundColor: '#424242',
+      },
+    },
+  };
+}
 
 const mapDispatchToProps = dispatch => ({
   addComponent: ({ title }) => dispatch(actions.addComponent({ title })),
-  updateComponent: ({
-    id,
-    index,
-    newParentId = null,
-    color = null,
-    stateful = null
-  }) =>
+  updateComponent: ({ id, index, newParentId = null, color = null, stateful = null }) =>
     dispatch(
       actions.updateComponent({
         id,
         index,
         newParentId,
         color,
-        stateful
-      })
+        stateful,
+      }),
     ),
   // deleteComponent: ({ index, id, parentIds }) => dispatch(actions.deleteComponent({ index, id, parentIds })),
   // moveToBottom: componentId => dispatch(actions.moveToBottom(componentId)),
@@ -36,27 +73,25 @@ const mapDispatchToProps = dispatch => ({
   // openExpansionPanel: component => dispatch(actions.openExpansionPanel(component)),
   // deleteAllData: () => dispatch(actions.deleteAllData()),
   addChild: ({ title }) => dispatch(actions.addChild({ title })),
-  changeFocusComponent: ({ title }) =>
-    dispatch(actions.changeFocusComponent({ title })),
-  changeFocusChild: ({ title, childId }) =>
-    dispatch(actions.changeFocusChild({ title, childId }))
+  changeFocusComponent: ({ title }) => dispatch(actions.changeFocusComponent({ title })),
+  changeFocusChild: ({ title, childId }) => dispatch(actions.changeFocusChild({ title, childId })),
 });
 
 class LeftContainer extends Component {
   state = {
-    componentName: ""
+    componentName: '',
   };
 
   handleChange = event => {
     this.setState({
-      [event.target.name]: event.target.value
+      [event.target.name]: event.target.value,
     });
   };
 
   handleAddComponent = () => {
     this.props.addComponent({ title: this.state.componentName });
     this.setState({
-      componentName: ""
+      componentName: '',
     });
   };
 
@@ -71,7 +106,7 @@ class LeftContainer extends Component {
       addChild,
       changeFocusComponent,
       changeFocusChild,
-      selectableChildren
+      selectableChildren,
     } = this.props;
     const { componentName } = this.state;
 
@@ -100,13 +135,13 @@ class LeftContainer extends Component {
           <Grid item xs={10}>
             <TextField
               id="title-input"
-              label="Add a new component"
-              placeholder="AppComponent"
+              label="Add class component"
+              placeholder="Name of component"
               margin="normal"
               autoFocus
               onChange={this.handleChange}
               onKeyPress={ev => {
-                if (ev.key === "Enter") {
+                if (ev.key === 'Enter') {
                   // Do code here
                   this.handleAddComponent();
                   ev.preventDefault();
@@ -116,10 +151,10 @@ class LeftContainer extends Component {
               name="componentName"
               className={classes.light}
               InputProps={{
-                className: classes.input
+                className: classes.input,
               }}
               InputLabelProps={{
-                className: classes.input
+                className: classes.input,
               }}
             />
           </Grid>
@@ -138,6 +173,9 @@ class LeftContainer extends Component {
           </Grid>
         </Grid>
         <div className="expansionPanel">{componentsExpansionPanel}</div>
+        <div className={'htmlPanel'}>
+          <HTMLComponentPanel focusComponent={focusComponent} addChild={addChild} />
+        </div>
       </div>
     );
   }
@@ -147,8 +185,8 @@ export default compose(
   withStyles(styles),
   connect(
     null,
-    mapDispatchToProps
-  )
+    mapDispatchToProps,
+  ),
 )(LeftContainer);
 
 // LeftContainer.propTypes = {
@@ -164,44 +202,3 @@ export default compose(
 //   totalComponents: PropTypes.number.isRequired,
 //   classes: PropTypes.object,
 // };
-
-function styles() {
-  return {
-    cssLabel: {
-      color: "white",
-
-      "&$cssFocused": {
-        color: "green"
-      }
-    },
-    cssFocused: {},
-    input: {
-      color: "#fff",
-      opacity: "0.7",
-      marginBottom: "10px"
-    },
-    underline: {
-      color: "white",
-      "&::before": {
-        color: "white"
-      }
-    },
-    button: {
-      color: "#fff",
-
-      "&:disabled": {
-        color: "grey"
-      }
-    },
-    clearButton: {
-      top: "96%",
-      position: "sticky!important",
-      zIndex: "1",
-
-      "&:disabled": {
-        color: "grey",
-        backgroundColor: "#424242"
-      }
-    }
-  };
-}
