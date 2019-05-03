59ce481a99468cc5277fcd741acd5a8253c7812a
ChristianEdwardPadilla <christianepadilla@gmail.com>
2019-05-03 12:19:19 -0400

    style changes

--- src/utils/createApplication.util.js
+++ src/utils/createApplication.util.js
@@ -10,42 +10,46 @@
 import fs from 'fs';
 import { format } from 'prettier';
 
-function createApplicationUtil({path, appName, genOption, repoUrl}) {
-  if (genOption === 1) {
-    return [
-      function createIndexHtml() {
-        const fileName = 'index.html';
-        const data = `
-          <!DOCTYPE html>
-          <html lang="en">
-          <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <meta http-equiv="X-UA-Compatible" content="ie=edge">
-            <title>Document</title>
-          </head>
-          <body>
-            <div id='root'></div>
-            <script src='bundle.js'></script>
-          </body>
-          </html>
-        `;
-        fs.writeFile(fileName, format(data, {
-          singleQuote: true,
-          trailingComma: 'es5',
-          bracketSpacing: true,
-          jsxBracketSameLine: true,
-          parser: 'babel',
-        }),
-        (err) => {
-          if (err) {
-            console.log('index.html error:', err.message);
-          } else {
-            console.log('index.html written successfully');
-          }
-        });
+function createApplicationUtil({
+  path, appName, genOption, repoUrl,
+}) {
+  function createIndexHtml() {
+    const fileName = 'index.html';
+    const data = `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <meta http-equiv="X-UA-Compatible" content="ie=edge">
+          <title>Document</title>
+        </head>
+        <body>
+          <div id='root'></div>
+          <script src='bundle.js'></script>
+        </body>
+        </html>
+      `;
+    fs.writeFile(
+      fileName,
+      format(data, {
+        singleQuote: true,
+        trailingComma: 'es5',
+        bracketSpacing: true,
+        jsxBracketSameLine: true,
+        parser: 'babel',
+      }),
+      (err) => {
+        if (err) {
+          console.log('index.html error:', err.message);
+        } else {
+          console.log('index.html written successfully');
+        }
       },
-    ];
+    );
+  }
+  if (genOption === 1) {
+    createIndexHtml();
   }
 }
 export default createApplicationUtil;
@@ -62,14 +66,8 @@ export default createApplicationUtil;
 //       await execFile('touch', '.babelrc', { cwd: path }),
 //     ];
 //   }
-  // return repoUrl ? execFile('git', ['clone', repoUrl, appName], { cwd: path }) : null;
+// return repoUrl ? execFile('git', ['clone', repoUrl, appName], { cwd: path }) : null;
 // }
-
-
-
-
-
-
 
 // import util from 'util';
 
@@ -79,7 +77,6 @@ export default createApplicationUtil;
 // cosnt genOptions = [
 //   'Export into existing project.', 'Export with starter repo', 'Export with create-react-app.'
 // ];
-
 
 // async function createApplicationUtil({
 //   path, appName, genOption, repoUrl,
