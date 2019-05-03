d377b1ea59c238574ed6284c5ce129989fd1869f
Adam <spincycle01@yahoo.com>
2019-05-03 11:52:52 -0400

    currently converted 3 files to TypeScript. Need to test on a different computer to verify it is working

--- src/utils/componentRender.util.js
+++ src/utils/componentRender.util.js
@@ -1,18 +1,22 @@
 const componentRender = (component, data) => {
   const {
-    stateful,
-    id,
-    position,
-    childrenArray,
-    title,
-    props,
+    stateful, id, position, childrenArray, title, props,
   } = component;
 
+  // need to filter with reduce the import, copy from below
   if (stateful) {
     return `
       import React, { Component } from 'react';
-      import PropTypes from 'prop-types';
-      ${childrenArray.map(child => `import ${child.componentName} from './${child.componentName}.tsx'`).join('\n')}
+      ${childrenArray
+    .map(child => `import ${child.componentName} from './${child.componentName}.tsx'`)
+    .reduce((acc, child) => {
+      if (!acc.includes(child)) {
+        acc.push(child);
+        return acc;
+      }
+      return acc;
+    }, [])
+    .join('\n')}
       
       class ${title} extends Component {
       constructor(props) {
@@ -29,32 +33,33 @@ const componentRender = (component, data) => {
         }
       }
 
-      ${title}.propTypes = {
-        ${props.map(p => `${p.key}: PropTypes.${p.type}${p.required ? '.isRequired' : ''},`).join('\n')}
-      }
-
       export default ${title};
     `;
   }
 
   return `
     import React from 'react';
-    import PropTypes from 'prop-types';
-    ${childrenArray.map(child => `import ${child.componentName} from './${child.componentName}.tsx'`).join('\n')}
+    ${childrenArray
+    .map(child => `import ${child.componentName} from './${child.componentName}.tsx'`)
+    .reduce((acc, child) => {
+      if (!acc.includes(child)) {
+        acc.push(child);
+        return acc;
+      }
+      return acc;
+    }, [])
+    .join('\n')}
     
 
     type Props = {
-      
+      ${component.props.map(prop => `${prop.key}: ${prop.type}`).join('\n')}
     }
-    const ${title} = props => (
+
+    const ${title} = (props: Props) => (
       <div>
         ${childrenArray.map(child => `<${child.componentName}/>`).join('\n')}
       </div>
     );
-
-    ${title}.propTypes = {
-      ${props.map(p => `${p.key}: PropTypes.${p.type}${p.required ? '.isRequired' : ''},`).join('\n')}
-    }
 
     export default ${title};
   `;
