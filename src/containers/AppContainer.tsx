602cedcd1b7332d2c6e0381a7d643bac2ec8f616
Shlomo <porges.s@gmail.com>
2019-05-07 19:32:58 -0400

    xd c=fileCOnvertIdsToObj

--- src/containers/AppContainer.tsx
+++ src/containers/AppContainer.tsx
@@ -1,14 +1,11 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-// import PropTypes from 'prop-types';
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import LinearProgress from '@material-ui/core/LinearProgress';
 import LeftContainer from './LeftContainer';
 import MainContainer from './MainContainer';
-// import RightContainer from './RightContainer.jsx';
-// import convertIdsToObjs from '../utils/convertIdsToObjs.util';
-import theme from '../components/theme.ts';
-import { loadInitData } from '../actions/components';
+import theme from '../components/theme';
+import { loadInitData } from '../actions/components.js';
 
 const mapStateToProps = store => ({
   components: store.workspace.components,
