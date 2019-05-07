75f2ffc683248fb67a42351d550e7fbe59b3eebb
Adam <spincycle01@yahoo.com>
2019-05-07 16:27:45 -0400

    App updated

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 import '../public/styles/style.css';
-<<<<<<< HEAD
 // import { MuiThemeProvider } from '@material-ui/core/styles';
-=======
->>>>>>> 7b32d8f43c0f8513994ea79b750f9cffc98844a1
 import AppContainer from '../containers/AppContainer';
 
 export const App: React.SFC = () => (
