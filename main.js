5f2053e545f3829a222b4cd60cb145d875009e58
ChristianEdwardPadilla <christianepadilla@gmail.com>
2019-05-10 10:53:09 -0400

    code exporting successfully

--- main.js
+++ main.js
@@ -1,24 +1,14 @@
 const path = require('path');
 
 const {
-  app,
-  BrowserWindow,
-  Menu,
-  shell,
-  dialog,
-  ipcMain
-} = require("electron");
+  app, BrowserWindow, Menu, shell, dialog, ipcMain,
+} = require('electron');
 
 // Uncomment below for hot reloading during development
-<<<<<<< HEAD
-require("electron-reload")(__dirname);
-=======
 // require('electron-reload')(__dirname);
->>>>>>> 11e0643d114b6c294dc2b66c3fd4727ffb173825
 
 // const isDev = true;
-const isDev =
-  process.env.NODE_ENV === "development" || process.env.NODE_ENV === "test";
+const isDev = process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test';
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -28,13 +18,13 @@ let mainWindow;
 function openFile() {
   // Opens file dialog looking for markdown
   const files = dialog.showOpenDialog(mainWindow, {
-    properties: ["openFile"],
+    properties: ['openFile'],
     filters: [
       {
-        name: "Images",
-        extensions: ["jpeg", "jpg", "png", "gif", "pdf"]
-      }
-    ]
+        name: 'Images',
+        extensions: ['jpeg', 'jpg', 'png', 'gif', 'pdf'],
+      },
+    ],
   });
 
   // if no files
@@ -42,39 +32,39 @@ function openFile() {
   const file = files[0];
 
   // Send fileContent to renderer
-  mainWindow.webContents.send("new-file", file);
+  mainWindow.webContents.send('new-file', file);
 }
 
 // Choose directory
-ipcMain.on("choose_app_dir", event => {
+ipcMain.on('choose_app_dir', (event) => {
   const directory = dialog.showOpenDialog(mainWindow, {
-    properties: ["openDirectory"],
-    buttonLabel: "Export"
+    properties: ['openDirectory'],
+    buttonLabel: 'Export',
   });
 
   if (!directory) return;
-  event.sender.send("app_dir_selected", directory[0]);
+  event.sender.send('app_dir_selected', directory[0]);
 });
 
-ipcMain.on("view_app_dir", (event, appDir) => {
+ipcMain.on('view_app_dir', (event, appDir) => {
   shell.openItem(appDir);
 });
 
 // Update file
-ipcMain.on("update-file", () => {
+ipcMain.on('update-file', () => {
   openFile();
 });
 
 const createWindow = () => {
   // Create the browser window.
   // eslint-disable-next-line
-  const { width, height } = require("electron").screen.getPrimaryDisplay().size;
+  const { width, height } = require('electron').screen.getPrimaryDisplay().size;
   mainWindow = new BrowserWindow({
     width,
     height,
     webPreferences: {
       zoomFactor: 0.7,
-      "node-Integration": false
+      'node-Integration': false,
     },
     show: false,
     icon: path.join(__dirname, '/src/public/icons/mac/icon.icns'),
@@ -87,22 +77,22 @@ const createWindow = () => {
   // and load the index.html of the app.
   mainWindow.loadURL(`file://${__dirname}/build/index.html`);
   // load page once window is loaded
-  mainWindow.once("ready-to-show", () => {
+  mainWindow.once('ready-to-show', () => {
     mainWindow.show();
   });
 
   const template = [
     {
-      label: "File",
+      label: 'File',
       submenu: [
         {
-          label: "Open File",
-          accelerator: process.platform === "darwin" ? "Cmd+O" : "Ctrl+Shift+O",
+          label: 'Open File',
+          accelerator: process.platform === 'darwin' ? 'Cmd+O' : 'Ctrl+Shift+O',
           click() {
             openFile();
-          }
-        }
-      ]
+          },
+        },
+      ],
     },
     // {
     //   label: 'Edit',
@@ -119,82 +109,81 @@ const createWindow = () => {
     //   ],
     // },
     {
-      label: "View",
+      label: 'View',
       submenu: [
-        { role: "reload" },
-        { role: "forcereload" },
-        { type: "separator" },
-        { role: "resetzoom" },
-        { role: "zoomin" },
-        { role: "zoomout" },
-        { type: "separator" },
-        { role: "togglefullscreen" }
-      ]
+        { role: 'reload' },
+        { role: 'forcereload' },
+        { type: 'separator' },
+        { role: 'resetzoom' },
+        { role: 'zoomin' },
+        { role: 'zoomout' },
+        { type: 'separator' },
+        { role: 'togglefullscreen' },
+      ],
     },
     {
-      role: "window",
-      submenu: [{ role: "minimize" }, { role: "close" }]
+      role: 'window',
+      submenu: [{ role: 'minimize' }, { role: 'close' }],
     },
     {
-      role: "help",
+      role: 'help',
       submenu: [
         {
-          label: "Learn More",
+          label: 'Learn More',
           click() {
-            shell.openExternal("https://electronjs.org");
-          }
-        }
-      ]
+            shell.openExternal('https://electronjs.org');
+          },
+        },
+      ],
     },
     {
-      label: "Developer",
+      label: 'Developer',
       submenu: [
         {
-          label: "Toggle Developer Tools",
-          accelerator:
-            process.platform === "darwin" ? "Alt+Command+I" : "Ctrl+Shift+I",
+          label: 'Toggle Developer Tools',
+          accelerator: process.platform === 'darwin' ? 'Alt+Command+I' : 'Ctrl+Shift+I',
           click() {
             mainWindow.webContents.toggleDevTools();
-          }
-        }
-      ]
-    }
+          },
+        },
+      ],
+    },
   ];
 
-  if (process.platform === "darwin") {
+  if (process.platform === 'darwin') {
     template.unshift({
       label: app.getName(),
       submenu: [
-        { role: "about" },
-        { type: "separator" },
-        { role: "services", submenu: [] },
-        { type: "separator" },
-        { role: "hide" },
-        { role: "hideothers" },
-        { role: "unhide" },
-        { type: "separator" },
-        { role: "quit" }
-      ]
+        { role: 'about' },
+        { type: 'separator' },
+        { role: 'services', submenu: [] },
+        { type: 'separator' },
+        { role: 'hide' },
+        { role: 'hideothers' },
+        { role: 'unhide' },
+        { type: 'separator' },
+        { role: 'quit' },
+      ],
     });
 
     // Edit menu
     template[2].submenu.push(
       {
-        type: "separator"
+        type: 'separator',
       },
       {
-        label: "Speech",
-        submenu: [{ role: "startspeaking" }, { role: "stopspeaking" }]
-      }
+        label: 'Speech',
+        submenu: [{ role: 'startspeaking' }, { role: 'stopspeaking' }],
+      },
     );
 
     // Window menu
     template[4].submenu = [
-      { role: "close" },
-      { role: "minimize" },
-      { role: "zoom" },
-      { type: "separator" },
-      { role: "front" }
+      { role: 'close' },
+      { role: 'minimize' },
+      { role: 'zoom' },
+      { type: 'separator' },
+      { role: 'front' },
     ];
   }
 
@@ -202,7 +191,7 @@ const createWindow = () => {
   Menu.setApplicationMenu(menu);
 
   // Emitted when the window is closed.
-  mainWindow.on("closed", () => {
+  mainWindow.on('closed', () => {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
@@ -213,13 +202,13 @@ const createWindow = () => {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on("ready", () => {
+app.on('ready', () => {
   if (isDev) {
     const {
       default: installExtension,
       REACT_DEVELOPER_TOOLS,
-      REDUX_DEVTOOLS
-    } = require("electron-devtools-installer");
+      REDUX_DEVTOOLS,
+    } = require('electron-devtools-installer');
 
     installExtension([REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS])
       .then(() => {
@@ -232,15 +221,15 @@ app.on("ready", () => {
 });
 
 // Quit when all windows are closed.
-app.on("window-all-closed", () => {
+app.on('window-all-closed', () => {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== "darwin") {
+  if (process.platform !== 'darwin') {
     app.quit();
   }
 });
 
-app.on("activate", () => {
+app.on('activate', () => {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
