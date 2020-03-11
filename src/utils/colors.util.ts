cf2879fa2c4926304fc69b1f073e8ceba64e7c21
Jesse Zuniga <jzuniga206@gmail.com>
2020-03-11 15:11:30 -0700

    Revert "Updated styling of component panels"
    This reverts commit cc715409a818db76018c7f39b20b651e04a1b9d9.

--- src/utils/colors.util.ts
+++ src/utils/colors.util.ts
@@ -1,30 +1,29 @@
 const colors: Array<string> = [
-  '#e27d60',
-  '#575757',
-  '#e8a87c',
-  '#c38d9e',
-  '#41b3a3',
-  '#d12fa2',
-  '#f64c72',
-  '#c99261',
-  '#355586',
-  '#5cdb95',
-  '#7395ae',
-  '#981d5c',
-  '#556738',
-  '#45a29e',
-  '#d79922',
-  '#0a2aa9',
-  '#9f2b17',
-  '#e98074',
-  '#8860d0',
-  '#5ab9ea',
-  '#5860e9',
-  '#84ceeb',
-  '#61892f'
+  '#E27D60',
+  '#E3AFBC',
+  '#E8A87C',
+  '#C38D9E',
+  '#41B3A3',
+  '#D12FA2',
+  '#F64C72',
+  '#DAAD86',
+  '#8EE4AF',
+  '#5CDB95',
+  '#7395AE', // light grey
+  '#b90061',
+  '#AFD275',
+  '#45A29E',
+  '#D79922',
+  '#C5CBE3', // lightish grey
+  '#FFCB9A',
+  '#E98074',
+  '#8860D0',
+  '#5AB9EA',
+  '#5860E9',
+  '#84CEEB',
+  '#61892F',
 ];
 
-const getColor = (): string =>
-  colors[Math.floor(Math.random() * colors.length)];
+const getColor = (): string => colors[Math.floor(Math.random() * colors.length)];
 
 export default getColor;
