204916a605e3372a84115013bc2549b76fa5826b
Tony Ito <tonyitocole@gmail.com>
2020-03-17 14:11:13 -0700

    Full tutorial funcctionality added

---
+++ src/components/Tutorial.tsx
@@ -0,0 +1,214 @@
+import React, { Component } from 'react';
+import Backdrop from '@material-ui/core/Backdrop';
+import Modal from '@material-ui/core/Modal';
+import Fade from '@material-ui/core/Fade';
+import theme from '../components/theme';
+import Button from '@material-ui/core/Button';
+
+interface Props {
+  tutorial: number;
+  handleClose(): void;
+  handleNext(tutorial: number): void;
+}
+
+class Tutorial extends Component<Props> {
+  render(): JSX.Element {
+    const { tutorial, handleClose, handleNext } = this.props;
+
+    let dialog;
+    if (!tutorial) dialog = (<div />);
+    if (tutorial === 1) {
+      dialog = (
+        <div
+          style={{
+            gridColumnStart: 'col2',
+            gridColumnEnd: 'col4',
+            gridRowStart: 'row9',
+            gridRowEnd: 'row10',
+            backgroundColor: theme.palette.background.paper,
+            border: '2px solid #000',
+            padding: '20px',
+            boxShadow: theme.shadows[5],
+            wordWrap: 'break-word',
+            width: '100%',
+            borderRadius: '20px 50px',
+          }}
+        >
+          <h1
+            style={{ color: theme.palette.primary.dark }}
+            id="transition-modal-title"
+          >
+            Step 1
+          </h1>
+          <h2 id="transition-modal-description">
+            Upload an image of your website template using the red 'upload image' button below.
+          </h2>
+          <h2 id="transition-modal-description">
+            You can remove the image by clicking the button again, or replace
+            the image by going to 'File --> Open Image'.
+          </h2>
+          <Button
+            onClick={() => {
+              handleNext(2);
+            }}
+            variant="contained"
+            color="inherit"
+            style={{ float: 'right' }}
+          >
+            Next
+          </Button>
+        </div>
+      );
+    }
+    if (tutorial === 2) {
+      dialog = (
+        <div
+          style={{
+            gridColumnStart: 'col3',
+            gridColumnEnd: 'col6',
+            gridRowStart: 'row2',
+            gridRowEnd: 'row3',
+            backgroundColor: theme.palette.background.paper,
+            border: '2px solid #000',
+            padding: '20px',
+            boxShadow: theme.shadows[5],
+            wordWrap: 'break-word',
+            width: '100%',
+            borderRadius: '20px 50px',
+          }}
+        >
+          <h1
+            style={{ color: theme.palette.primary.dark }}
+            id="transition-modal-title"
+          >
+            Step 2
+          </h1>
+          <h2 id="transition-modal-description">
+            Give your component a name add click '+' to add it to your collection of components.
+          </h2>
+          <h2 id="transition-modal-description">
+            Once you've added your component, while in the view of the component you would like to make the parent of your new component, click '+' next to the new component you created to make it a child. 
+          </h2>
+          <Button
+            onClick={() => {
+              handleNext(3);
+            }}
+            variant="contained"
+            color="inherit"
+            style={{ float: 'right' }}
+          >
+            Next
+          </Button>
+        </div>
+      );
+    }
+    if (tutorial === 3) {
+      dialog = (
+        <div
+          style={{
+            gridColumnStart: 'col6',
+            gridColumnEnd: 'col9',
+            gridRowStart: 'row9',
+            gridRowEnd: 'row10',
+            backgroundColor: theme.palette.background.paper,
+            border: '2px solid #000',
+            padding: '20px',
+            boxShadow: theme.shadows[5],
+            wordWrap: 'break-word',
+            width: '100%',
+            borderRadius: '20px 50px',
+          }}
+        >
+          <h1
+            style={{ color: theme.palette.primary.dark }}
+            id="transition-modal-title"
+          >
+            Step 3
+          </h1>
+          <h2 id="transition-modal-description">
+            Once you have designed your components above, you can add props and HTML elements to your components in this bottom panel.
+          </h2>
+          <h2 id="transition-modal-description">
+            While you're working on designing your app, you can also preview the code that will be exported and your application's component tree in the corresponding tabs. 
+          </h2>
+          <Button
+            onClick={() => {
+              handleNext(4);
+            }}
+            variant="contained"
+            color="inherit"
+            style={{ float: 'right' }}
+          >
+            Next
+          </Button>
+        </div>
+      );
+    }
+    if (tutorial === 4) {
+      dialog = (
+        <div
+          style={{
+            gridColumnStart: 'col1',
+            gridColumnEnd: 'col4',
+            gridRowStart: 'row9',
+            gridRowEnd: 'row10',
+            backgroundColor: theme.palette.background.paper,
+            border: '2px solid #000',
+            padding: '20px',
+            boxShadow: theme.shadows[5],
+            wordWrap: 'break-word',
+            width: '100%',
+            borderRadius: '20px 50px',
+          }}
+        >
+          <h1
+            style={{ color: theme.palette.primary.dark }}
+            id="transition-modal-title"
+          >
+            Step 4
+          </h1>
+          <h2 id="transition-modal-description">
+            Once you're all done, simply click on the "Export Project" button below to export your files for development.
+          </h2>
+          <h2 id="transition-modal-description">
+            You have the option of exporting just the components, or an entire React + Express boilerplate with the components you designed. 
+          </h2>
+          <Button
+            onClick={() => {
+              handleClose();
+            }}
+            variant="contained"
+            color="inherit"
+            style={{ float: 'right' }}
+          >
+            Finish!
+          </Button>
+        </div>
+      );
+    }
+    return (
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        open={tutorial !== 0}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+        style={{
+          display: 'grid',
+          gridTemplateColumns:
+            '[col1] 1fr [col2] 1fr [col3] 1fr [col4] 1fr [col5] 1fr [col6] 1fr [col7] 1fr [col8] 1fr [col9] 1fr [col10] 1fr [end]',
+          gridTemplateRows:
+            '[row1] 1fr [row2] 1fr [row3] 1fr [row4] 1fr [row5] 1fr [row6] 1fr [row7] 1fr [row8] 1fr [row9] 1fr [row10] 1fr [end]',
+        }}
+      >
+        <Fade in={tutorial !== 0}>{dialog}</Fade>
+      </Modal>
+    );
+  }
+}
+
+export default Tutorial;
--- src/containers/AppContainer.tsx
+++ src/containers/AppContainer.tsx
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { MuiThemeProvider } from '@material-ui/core/styles';
-import Modal from '@material-ui/core/Modal';
-import Fade from '@material-ui/core/Fade'
 import LinearProgress from '@material-ui/core/LinearProgress';
-import Backdrop from '@material-ui/core/Backdrop';
+import Tutorial from '../components/Tutorial';
 import LeftContainer from './LeftContainer';
 import MainContainer from './MainContainer';
 import theme from '../components/theme';
 // import { loadInitData } from '../actions/components.ts';
-import { ComponentInt, ComponentsInt, ApplicationStateInt, Action } from '../utils/Interfaces';
+import {
+  ComponentInt,
+  ComponentsInt,
+  ApplicationStateInt,
+} from '../utils/Interfaces';
 import * as actions from '../actions/components';
 
 // ** Used with electron to render
@@ -23,31 +25,31 @@ interface Props {
   focusComponent: ComponentInt;
   totalComponents: number;
   loading: boolean;
-  selectableChildren: number[]; 
+  selectableChildren: number[];
   loadInitData(): void;
   changeImagePath(imageSource: string): void;
-};
+}
 
 //Type for the state that should not be assigned within the
-//component below. 
+//component below.
 interface State {
   image: HTMLImageElement | null;
-  tutorial: boolean;
+  tutorial: number;
   changed: boolean;
-};
+}
 
 //Details on some of these are listed in the render where they are passed down.
-const mapStateToProps = (store: {workspace: ApplicationStateInt}) => ({
-  imageSource: store.workspace.imageSource, 
+const mapStateToProps = (store: { workspace: ApplicationStateInt }) => ({
+  imageSource: store.workspace.imageSource,
   components: store.workspace.components,
   totalComponents: store.workspace.totalComponents,
   focusComponent: store.workspace.focusComponent,
   loading: store.workspace.loading,
-  selectableChildren: store.workspace.selectableChildren
+  selectableChildren: store.workspace.selectableChildren,
 });
 
 //Dispatch functions for loading data where user left off
-//when they closed the app, and to change the path of the image 
+//when they closed the app, and to change the path of the image
 //if uploaded for template.
 
 //TODO: Create an interface for dispatch function
@@ -55,25 +57,23 @@ const mapStateToProps = (store: {workspace: ApplicationStateInt}) => ({
 const mapDispatchToProps = (dispatch: (arg: any) => void) => ({
   loadInitData: () => dispatch(actions.loadInitData()),
   changeImagePath: (imageSource: string) =>
-    dispatch(actions.changeImagePath(imageSource))
+    dispatch(actions.changeImagePath(imageSource)),
 });
-
-
 
 class AppContainer extends Component<Props, State> {
   constructor(props: Props) {
     super(props);
-    // THIS STATE SHOULD NOT EXIST HERE. 
+    // THIS STATE SHOULD NOT EXIST HERE.
     //First rule of Redux is to have a single source of truth, and state being assigned right
     //here breaks that rule if it gets pased down to any other component.
 
     //TODO: someone fix this pl0x (Possibly move to component that actually depends on it)
     this.state = {
       image: null,
-      tutorial: false,
-      changed: false
+      tutorial: 0,
+      changed: false,
     };
-    
+
     //This function is invoked upon a new file being uploaded to the app.
     //it changes the imagesource in the global state to be whatever filepath it is
     //on the user's comp.
@@ -84,37 +84,40 @@ class AppContainer extends Component<Props, State> {
       image.src = file;
       image.onload = () => {
         // update state when the image has been uploaded
-        this.props.changeImagePath(image.src); 
+        this.props.changeImagePath(image.src);
         this.setState({ image, changed: true });
       };
     });
     IPC.on('tutorial_clicked', () => {
-      this.setState({tutorial: true});
+      this.setState({ tutorial: 1 });
     });
   }
 
   handleClose = () => {
-    this.setState({tutorial: false});
+    this.setState({ tutorial: 0 });
   };
 
-  //This sets checks if the image was removed via the clear image button on the left container. 
-  //Technically this logic should be done in the reducer, not here. 
+  handleNext = (tutorial: number) => {
+    this.setState({ tutorial });
+  }
+
+  //This sets checks if the image was removed via the clear image button on the left container.
+  //Technically this logic should be done in the reducer, not here.
   componentDidUpdate(prevProps: Props) {
     const { imageSource } = this.props;
     const { changed } = this.state;
     if (imageSource === '' && changed) {
       this.setState({ image: null, changed: false });
-    } 
+    }
     // else if (imageSource !== prevProps.imageSource && imageSource !== '') {
     //   this.setImage(imageSource);
     // }
   }
 
   ///////////////////THIS BLOCK MIGHT NOT BE DOING ANYTHING ////////////////////////////
-  //This is a helper function for the lifecycle function above that I think was supposed 
-  // to set the image in the state based on the image source, and changes the set status to true, 
-  //also in the state. 
-
+  //This is a helper function for the lifecycle function above that I think was supposed
+  // to set the image in the state based on the image source, and changes the set status to true,
+  //also in the state.
 
   // setImage = (imageSource: string) => {
   //   if (imageSource) {
@@ -128,11 +131,10 @@ class AppContainer extends Component<Props, State> {
   //   }
   // };
 
-  //this will load the saved sata from last close 
+  //this will load the saved sata from last close
   componentDidMount() {
     this.props.loadInitData();
   }
-
 
   render(): JSX.Element {
     const {
@@ -140,44 +142,20 @@ class AppContainer extends Component<Props, State> {
       focusComponent,
       loading,
       selectableChildren,
-      totalComponents
+      totalComponents,
     } = this.props;
 
     // uses component childIds and parentIds arrays (numbers) to build component-filled children and parents arrays
     return (
-      <MuiThemeProvider theme={theme} //I'm assuming this is some material-UI theme thing
-      >  <Modal
-        aria-labelledby="transition-modal-title"
-        aria-describedby="transition-modal-description"
-        open={this.state.tutorial}
-        onClose={this.handleClose}
-        closeAfterTransition
-        BackdropComponent={Backdrop}
-        BackdropProps={{
-          timeout: 500,
-        }}
-        style={{
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
+      <MuiThemeProvider
+        theme={theme} //I'm assuming this is some material-UI theme thing
       >
-        <Fade in={this.state.tutorial}>
-          <div style={
-            {
-              backgroundColor: theme.palette.background.paper,
-              border: '2px solid #000',
-              boxShadow: theme.shadows[5],
-              // padding: theme.spacing(2, 4, 3)
-            }
-          }>
-            <h1 id="transition-modal-title">Welcome to the tutorial!</h1>
-            <h2 id="transition-modal-description">I made the tutorial modal, so now I'm just waiting on Eliot to finish making the actual step-by-step instructions.</h2>
-            <h2 id="transition-modal-description">If you're still seeing this message, you can blame Eliot! Please visit our site by clicking 'Learn More' under the help menu.</h2>
-          </div>
-        </Fade>
-      </Modal>
-        <div className='app-container'>
+        <Tutorial //Tutorial modal that is triggered upon selecting menu item
+          tutorial={this.state.tutorial}
+          handleClose={this.handleClose}
+          handleNext={this.handleNext}
+        />
+        <div className="app-container">
           <LeftContainer //The left side-bar that contains the component cards and the buttons.
             components={components}
             totalComponents={totalComponents}
@@ -188,18 +166,19 @@ class AppContainer extends Component<Props, State> {
             components={components}
             image={this.state.image}
             imageSource={this.props.imageSource}
-            classes = {null} //placeholder, for some reason it's expecting this prop
+            classes={null} //placeholder, for some reason it's expecting this prop
           />
           {loading ? ( //This is triggered when files are being exported. Unsure if it actually does anything.
             <div
               style={{
                 alignSelf: 'flex-end',
                 position: 'fixed',
-                width: '100%'
+                width: '100%',
               }}
             >
-              <LinearProgress color='secondary' //Pretty sure this is a loading bar component from Material-UI, 
-                                                //never seen it in action though.
+              <LinearProgress
+                color="secondary" //Pretty sure this is a loading bar component from Material-UI,
+                //never seen it in action though.
               />
             </div>
           ) : null}
