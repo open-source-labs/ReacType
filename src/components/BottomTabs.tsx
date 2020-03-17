da687fce053c7483d82f621667af055a5f0e8aa2
Admin <admin@Admins-MacBook-Pro.local>
2020-03-17 11:43:40 -0700

    Fixed React D3 type for "this.translate"

--- src/components/BottomTabs.tsx
+++ src/components/BottomTabs.tsx
@@ -1,12 +1,12 @@
-import React, { Component } from "react";
-import { withStyles } from "@material-ui/core/styles";
-import Tabs from "@material-ui/core/Tabs";
-import Tab from "@material-ui/core/Tab";
-import Tree from "react-d3-tree";
-import Props from "./Props.tsx";
-import HtmlAttr from "./HtmlAttr.tsx";
-import CodePreview from "./CodePreview.tsx";
-import { ComponentInt, ComponentsInt, ChildInt } from "../utils/Interfaces.ts";
+import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import { Tree, ReactD3TreeTranslate } from 'react-d3-tree';
+import Props from './Props';
+import HtmlAttr from './HtmlAttr';
+import CodePreview from './CodePreview';
+import { ComponentInt, ComponentsInt, ChildInt } from '../utils/Interfaces';
 
 interface PropsInt {
   focusChild: ChildInt;
@@ -26,45 +26,45 @@ interface TreeInt {
 const styles = (theme: any): any => ({
   root: {
     flexGrow: 1,
-    backgroundColor: "#333333",
-    height: "100%",
-    color: "#fff",
-    boxShadow: "0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)"
+    backgroundColor: '#333333',
+    height: '100%',
+    color: '#fff',
+    boxShadow: '0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)'
   },
   tabsRoot: {
-    borderBottom: "0.5px solid #424242"
+    borderBottom: '0.5px solid #424242'
   },
   tabsIndicator: {
-    backgroundColor: "#1de9b6"
+    backgroundColor: '#1de9b6'
   },
   tabRoot: {
-    textTransform: "initial",
+    textTransform: 'initial',
     minWidth: 72,
     fontWeight: theme.typography.fontWeightRegular,
     marginRight: theme.spacing.unit * 4,
 
     fontFamily: [
-      "-apple-system",
-      "BlinkMacSystemFont",
+      '-apple-system',
+      'BlinkMacSystemFont',
       '"Segoe UI"',
-      "Roboto",
+      'Roboto',
       '"Helvetica Neue"',
-      "Arial",
-      "sans-serif",
+      'Arial',
+      'sans-serif',
       '"Apple Color Emoji"',
       '"Segoe UI Emoji"',
       '"Segoe UI Symbol"'
-    ].join(","),
-    "&:hover": {
-      color: "#1de9b6",
+    ].join(','),
+    '&:hover': {
+      color: '#1de9b6',
       opacity: 1
     },
-    "&$tabSelected": {
-      color: "#33eb91",
+    '&$tabSelected': {
+      color: '#33eb91',
       fontWeight: theme.typography.fontWeightMedium
     },
-    "&:focus": {
-      color: "#4aedc4"
+    '&:focus': {
+      color: '#4aedc4'
     }
   },
   tabSelected: {},
@@ -80,6 +80,9 @@ class BottomTabs extends Component<PropsInt> {
   state = {
     value: 0
   };
+
+  treeWrapper: HTMLElement;
+  translate: ReactD3TreeTranslate;
 
   componentDidMount() {
     // dynamically center the tree based on the div size
@@ -97,11 +100,13 @@ class BottomTabs extends Component<PropsInt> {
   };
 
   generateComponentTree(componentId: number, components: ComponentsInt) {
-    const component = components.find(comp => comp.id === componentId);
+    const component = components.find(
+      (comp: ComponentInt) => comp.id === componentId
+    );
     const tree = { name: component.title, attributes: {}, children: [] };
 
-    component.childrenArray.forEach(child => {
-      if (child.childType === "COMP") {
+    component.childrenArray.forEach((child: ChildInt) => {
+      if (child.childType === 'COMP') {
         tree.children.push(
           this.generateComponentTree(child.childComponentId, components)
         );
@@ -129,8 +134,10 @@ class BottomTabs extends Component<PropsInt> {
 
     // display count on the tab. user can see without clicking into tab
     const propCount = focusComponent.props.length;
+
+    // this function is creating the HTML attribute count that you see in parenthese
     const htmlAttribCount = focusComponent.childrenArray.filter(
-      child => child.childType === "HTML"
+      (child: ChildInt) => child.childType === 'HTML'
     ).length;
 
     return (
@@ -143,38 +150,38 @@ class BottomTabs extends Component<PropsInt> {
           <Tab
             disableRipple
             classes={{ root: classes.tabRoot, selected: classes.tabSelected }}
-            label="Application Tree"
+            label='Application Tree'
           />
           <Tab
             disableRipple
             classes={{ root: classes.tabRoot, selected: classes.tabSelected }}
-            label="Code Preview"
+            label='Code Preview'
           />
           <Tab
             disableRipple
             classes={{ root: classes.tabRoot, selected: classes.tabSelected }}
-            label={`Component Props ${propCount ? `(${propCount})` : ""} `}
+            label={`Component Props ${propCount ? `(${propCount})` : ''} `}
           />
           <Tab
             disableRipple
             classes={{ root: classes.tabRoot, selected: classes.tabSelected }}
             label={`HTML Element Attributes ${
-              htmlAttribCount ? `(${htmlAttribCount})` : ""
+              htmlAttribCount ? `(${htmlAttribCount})` : ''
             } `}
           />
-          {/* <Tab
+          <Tab
             disableRipple
             classes={{ root: classes.tabRoot, selected: classes.tabSelected }}
-            label="Component State"
-          /> */}
+            label='Component State'
+          />
         </Tabs>
 
         {value === 0 && (
           <div
-            id="treeWrapper"
+            id='treeWrapper'
             style={{
-              width: "100%",
-              height: "100%"
+              width: '100%',
+              height: '100%'
             }}
             ref={node => (this.treeWrapper = node)}
           >
@@ -187,15 +194,15 @@ class BottomTabs extends Component<PropsInt> {
                 nodes: {
                   node: {
                     name: {
-                      fill: "#D3D3D3",
-                      stroke: "#D3D3D3",
+                      fill: '#D3D3D3',
+                      stroke: '#D3D3D3',
                       strokeWidth: 1
                     }
                   },
                   leafNode: {
                     name: {
-                      fill: "#D3D3D3",
-                      stroke: "#D3D3D3",
+                      fill: '#D3D3D3',
+                      stroke: '#D3D3D3',
                       strokeWidth: 1
                     }
                   }
@@ -211,11 +218,10 @@ class BottomTabs extends Component<PropsInt> {
           />
         )}
         {value === 2 && <Props />}
-        {value === 3 && focusChild.childType === "HTML" && <HtmlAttr />}
-        {value === 3 &&
-          focusChild.childType !== "HTML" && (
-            <p>Please select an HTML element to view attributes</p>
-          )}
+        {value === 3 && focusChild.childType === 'HTML' && <HtmlAttr />}
+        {value === 3 && focusChild.childType !== 'HTML' && (
+          <p>Please select an HTML element to view attributes</p>
+        )}
       </div>
     );
   }
