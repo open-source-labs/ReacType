28191d45cccd0f68b622fa6a55ec6fdddd948aea
Tony Ito <tonyitocole@gmail.com>
2020-04-02 14:16:32 -0700

    Added testing setup

--- src/setupTests.js
+++
@@ -1,8 +0,0 @@
-import Enzyme, { shallow, render, mount } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
-Enzyme.configure({ adapter: new Adapter() });
-
-global.shallow = shallow;
-global.render = render;
-global.mount = mount;
