321eac53185ea6b87f988f3406934298275d9f85
Jesse Zuniga <jzuniga206@gmail.com>
2020-04-04 12:32:48 -0700

    Implemented tests for class and state toggle reducers

--- src/reducers/__tests__/leftReducers.test.ts
+++ src/reducers/__tests__/leftReducers.test.ts
@@ -1,43 +1,70 @@
 import * as reducers from '../leftReducers';
-// import { initialApplicationState } from '../initialState';
+import { initialApplicationState } from '../initialState';
 
 describe('Left reducers', () => {
   let state: any;
 
   // redefine the default state before each reducer test
   beforeEach(() => {
-    state = {
-      editMode: -1,
-      testing: 'testingReducer',
-      codeReadOnly: true,
-      components: [
-        {
-          changed: true,
-          childrenArray: [{}],
-          classBased: false,
-          code: '....',
-          color: '#FF6D00',
-          focusChild: {},
-          focusChildId: -1,
-          id: 1,
-          nextChildId: 3,
-          nextPropId: 2,
-          position: {
-            height: 850,
-            width: 500,
-            x: 70,
-            y: 100
-          },
-          props: [],
-          stateful: false,
-          title: 'App'
-        }
-      ]
-    };
+    state = initialApplicationState;
+    // state = {
+    //   editMode: -1,
+    //   testing: 'testingReducer',
+    //   codeReadOnly: true,
+    //   components: [
+    //     {
+    //       changed: true,
+    //       childrenArray: [{}],
+    //       classBased: false,
+    //       code: '....',
+    //       color: '#FF6D00',
+    //       focusChild: {},
+    //       focusChildId: -1,
+    //       id: 1,
+    //       nextChildId: 3,
+    //       nextPropId: 2,
+    //       position: {
+    //         height: 850,
+    //         width: 500,
+    //         x: 70,
+    //         y: 100
+    //       },
+    //       props: [],
+    //       stateful: false,
+    //       title: 'App'
+    //     }
+    //   ]
+    // };
+  });
+
+  describe('toggleComponentClass', () => {
+    it('toggles the component passed in between class and functional', () => {
+      const action = {
+        type: 'TOGGLE_CLASS',
+        payload: { id: 1 }
+      };
+
+      const newState = reducers.toggleComponentClass(state, action.payload);
+      // type error below appears to be due to typing of the Components interface.. investigate later
+      expect(newState.components[0].classBased).toEqual(
+        !state.components[0].classBased
+      );
+    });
   });
 
   describe('toggleComponentState', () => {
-    it('inverts the statefulness of component passed in', () => {});
+    it('inverts the statefulness of component passed in', () => {
+      const action = {
+        type: 'TOGGLE_STATE',
+        payload: { id: 1 }
+      };
+
+      const newState = reducers.toggleComponentState(state, action.payload);
+      // type error below appears to be due to typing of the Components interface.. investigate later
+      expect(newState.components[0].stateful).toEqual(
+        !state.components[0].stateful
+      );
+    });
   });
 
   // toggleEditMode reducer allows changing of component names in left container
