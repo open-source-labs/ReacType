e81b85e6b0721323b8002f6342ab1bc45ec78d3b
andrewjcho84 <andrewjcho84@gmail.com>
2020-07-09 10:35:13 -0700

    server fixing for new merge

--- app/src/index.js
+++ app/src/index.js
@@ -27,15 +27,15 @@ const PrivateRoute = ({ component: Component, ...rest }) => (
   />
 );
 
-// ReactDOM.render(
-//   <Router>
-//     <Switch>
-//       <Route exact path="/login" component={SignIn} />
-//       <Route exact path="/signup" component={SignUp} />
-//       <PrivateRoute path="/" component={App} />
-//     </Switch>
-//   </Router>,
-//   document.getElementById('app')
-// );
+ReactDOM.render(
+  <Router>
+    <Switch>
+      <Route exact path="/login" component={SignIn} />
+      <Route exact path="/signup" component={SignUp} />
+      <PrivateRoute path="/" component={App} />
+    </Switch>
+  </Router>,
+  document.getElementById('app')
+);
 
-ReactDOM.render(<App></App>, document.getElementById('app'));
+// ReactDOM.render(<App></App>, document.getElementById('app'));
