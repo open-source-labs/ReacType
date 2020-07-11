d5b96357f505a7ffaeb833a1f86442f5497d7bd5
andrewjcho84 <andrewjcho84@gmail.com>
2020-07-11 14:13:35 -0700

    server routing based on node_env

--- app/src/components/login/LoginButton.tsx
+++ app/src/components/login/LoginButton.tsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import { stateContext } from '../../context/context';
 import Button from '@material-ui/core/Button';
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
-import { useHistory } from "react-router-dom";
+import { useHistory } from 'react-router-dom';
 
 export default function LoginButton() {
   let history = useHistory();
@@ -11,8 +11,12 @@ export default function LoginButton() {
   const handleLogout = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     e.preventDefault();
     console.log('Logout clicked, destroying cookie, redirect to login');
+    // clear local storage
+    window.localStorage.clear();
     // destroys cookie by backdating expiration time
     document.cookie = 'ssid=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+    // destroy cookie in production via electron main process
+    window.api.delCookie();
     // uses useHistory to return to the login page
     history.push('/login');
   };
@@ -28,7 +32,7 @@ export default function LoginButton() {
       >
         Log Out
       </Button>
-    )
+    );
   } else {
     return (
       <Button
@@ -40,6 +44,6 @@ export default function LoginButton() {
       >
         Log In
       </Button>
-    )
+    );
   }
 }
