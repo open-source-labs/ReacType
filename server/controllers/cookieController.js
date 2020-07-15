c92a2f88fafbfa5acc80b5d3978c69dc998bd505
andrewjcho84 <andrewjcho84@gmail.com>
2020-07-15 12:42:18 -0700

    modify server readme, add cookie security

--- server/controllers/cookieController.js
+++ server/controllers/cookieController.js
@@ -3,7 +3,11 @@ const cookieController = {};
 // setSSIDCookie - store the user id from database in cookie
 cookieController.setSSIDCookie = (req, res, next) => {
   // set cookie with key 'ssid' and value to user's id
-  res.cookie('ssid', res.locals.id);
+  res.cookie('ssid', res.locals.id, {
+    httpOnly: true,
+    sameSite: 'None',
+    secure: true
+  });
   return next();
 };
 
