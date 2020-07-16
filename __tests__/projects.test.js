4cd9ec9994a38200803085a04332def186cee760
Aaron Bumanglag <aaron.k.bumanglag@gmail.com>
2020-07-15 20:21:58 -0700

    modified endpoint tests

--- __tests__/projects.test.js
+++ __tests__/projects.test.js
@@ -6,7 +6,7 @@ if (isDev) {
 }
 
 // save and get projects endpoint testing
-describe('Project endpoints tests', () => {
+xdescribe('Project endpoints tests', () => {
   // initializes the project to be sent to server/DB
   const state = {
     name: 'test',
