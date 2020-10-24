b334e426bec59e3e30b50a1d0017a6b2190239ab
stormi <stormikhashimoto@gmail.com>
2020-10-24 12:40:14 -0700

    working

--- app/src/components/bottom/BottomPanel.tsx
+++ app/src/components/bottom/BottomPanel.tsx
@@ -11,7 +11,7 @@ const BottomPanel = () => {
     <Resizable
       enable={{
         top: true,
-      }}
+      }} minHeight={'25%'}
     >
       <div className="bottom-panel">
         <BottomTabs />
