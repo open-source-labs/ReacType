9a880c0eae61250fa6bd7d7f78e421b8664dbcb7
Brian <brianjshan@gmail.com>
2020-11-04 08:19:53 -0800

    👍customizing & styling tutorial

--- app/src/tutorial/Customization.tsx
+++ app/src/tutorial/Customization.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import { Link, withRouter, RouteComponentProps } from 'react-router-dom';
+import display from '../../../resources/customizing_elements_images/Display.png';
+import flex from '../../../resources/customizing_elements_images/Flex.png';
+import height from '../../../resources/customizing_elements_images/Height.png';
+import width from '../../../resources/customizing_elements_images/Width.png';
+import backgroundcolor from '../../../resources/customizing_elements_images/BackgroundColor.png';
 
 // import Grid from '@material-ui/core/Grid';
 
@@ -11,16 +16,50 @@ import { Link, withRouter, RouteComponentProps } from 'react-router-dom';
 const Customization: React.FC<RouteComponentProps> = () => {
   return (
     <div>
-      Customization
-      <Link to={`/`}>
-        <button>Application</button>
-      </Link>
       <Link to={`/tutorial`}>
-        <button>Tutorial</button>
+        <button>Back to Tutorial Page</button>
       </Link>
+      <hr />
+      <h1 style={{ color: 'black' }}>Customizing Elements</h1>
+      <br />
+      <h2>Display</h2>
+      <br />
+      <img src={display} />
+      <p>
+        After having moved a page, component, route link, or element into the
+        canvas, select the one that needs customizing simply by clicking on it.
+        Then, to give it a display feature of either a block, inline-block, or
+        flex styling, select from the drop down box.
+        <br />
+        <br />
+        <img src={flex} />
+        <br />
+        <br />
+        If the display option 'flex' is chosen, few more sub-options are
+        displayed under the display option.
+      </p>
+      <br />
+      <h2>Width</h2>
+      <img src={width} />
+      <br />
+      <p>Change the width of each component, route link, or element.</p>
+      <br />
+      <h2>Height</h2>
+      <img src={height} />
+      <br />
+      <p>Change the height of each component, route link, or element.</p>
+      <br />
+      <h2>Background Color</h2>
+      <img src={backgroundcolor} />
+
+      <br />
+      <p>
+        Select an element, type in the color you wish to change the background
+        color to, and click save!
+      </p>
+      <br />
     </div>
   );
 };
 
 export default withRouter(Customization);
-
--- app/src/tutorial/Styling.tsx
+++ app/src/tutorial/Styling.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import { Link, withRouter, RouteComponentProps } from 'react-router-dom';
+import theme from '../../../resources/customizing_elements_images/Theme.png';
+import lighting from '../../../resources/customizing_elements_images/Lighting.png';
+import resize from '../../../resources/customizing_elements_images/Resize.png';
+import codechange from '../../../resources/customizing_elements_images/CodeChange.png';
 
 // import Grid from '@material-ui/core/Grid';
 
@@ -11,16 +15,46 @@ import { Link, withRouter, RouteComponentProps } from 'react-router-dom';
 const Styling: React.FC<RouteComponentProps> = () => {
   return (
     <div>
-      Styling
-      <Link to={`/`}>
-        <button>Application</button>
-      </Link>
       <Link to={`/tutorial`}>
-        <button>Tutorial</button>
+        <button>Back to Tutorial Page</button>
       </Link>
+      <br />
+      <h1 style={{ color: 'black' }}>Styling Features</h1>
+      <h2>Code Preview Theme Changer</h2>
+      <img src={theme} />
+      <br />
+      <p>
+        Select your favorite theme from the drop down menu to personalize your
+        view of the code preview!
+      </p>
+      <br />
+      <h2>Lighting Mode</h2>
+      <img src={lighting} />
+      <br />
+      <p>
+        Spice up the app by toggling between different lighting modes! The
+        lighting mode will change the background color of the app as well as the
+        background color of the component tree.
+      </p>
+      <br />
+      <h2>Resize Code Preview & Component Tree</h2>
+      <img src={resize} />
+      <br />
+      <p>
+        Hover over the line above the code preview and/or component tree to
+        resize the section. Simply click and drag up or down to resize.
+      </p>
+      <br />
+      <h2>Customize Code Preview</h2>
+      <img src={codechange} />
+      <br />
+      <p>
+        Change your code before exporting and see the changes in your exported
+        file!
+      </p>
+      <br />
     </div>
   );
 };
 
 export default withRouter(Styling);
-
