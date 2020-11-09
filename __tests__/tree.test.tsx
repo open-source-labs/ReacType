06aed17290d17263fbd466401b17984728f0659b
stormi <stormikhashimoto@gmail.com>
2020-11-09 07:10:55 -0800

    tree testing

---
+++ __tests__/tree.test.tsx
@@ -0,0 +1,33 @@
+import tree from '../app/src/tree/TreeChart';
+
+xdescribe('Testing the tree functionality', function () {
+
+  //TEST 'ADD COMPONENT TO PAGE'
+  xdescribe('ADD COMPONENT TO PAGE', () => {
+    it('should add a new reusable component to page', () => {
+
+    })
+  })
+
+  //TEST 'ADD COMPONENT TO ANOTHER COMPONENT'
+  xdescribe('ADD COMPONENT TO ANOTHER COMPONENT', () => {
+    it('should add child component to top-level component', () => {
+
+    })
+  })
+
+  //TEST 'ADD A HTML ELEMENT TO A PAGE'
+  xdescribe('ADD CHILD TO COMPONENT', () => {
+    it('should add child component to top-level component', () => {
+
+    })
+  })
+
+  //TEST 'ADD A HTML ELEMENT TO A COMPONENT'
+  xdescribe('', () => {
+    it('', () => {
+
+    })
+  })
+
+})
