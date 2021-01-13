2e32546927258f075dd19a1b0e11754030ae7d2c
Linh Tran <linhtanl51@gmail.com>
2021-01-12 20:19:09 -0600

    take out unused imported modules

--- app/src/index.js
+++ app/src/index.js
@@ -1,23 +1,21 @@
 import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App.tsx';
 import Cookies from 'js-cookie';
 
+import App from './components/App.tsx';
 import SignIn from './components/login/SignIn.tsx';
 import SignUp from './components/login/SignUp.tsx';
 import FBPassWord from './components/login/FBPassWord.tsx';
 import Tutorial from './tutorial/Tutorial.tsx';
 import TutorialPage from './tutorial/TutorialPage.tsx';
 
-
 import {
   HashRouter as Router,
   Route,
   Redirect,
-  Switch,
+  Switch
 } from 'react-router-dom';
-
 
 const PrivateRoute = ({ component: Component, ...rest }) => (
   <Route
@@ -25,11 +23,10 @@ const PrivateRoute = ({ component: Component, ...rest }) => (
     render={props => {
       return Cookies.get('ssid') || window.localStorage.getItem('ssid') ? (
         <Component {...props} />
-        ) : (
-          <Redirect to="/login" />
-          )
-      }
-    }
+      ) : (
+        <Redirect to="/login" />
+      );
+    }}
   />
 );
 
@@ -40,7 +37,7 @@ ReactDOM.render(
       <Route exact path="/signup" component={SignUp} />
       <Route exact path="/password" component={FBPassWord} />
       <PrivateRoute exact path="/" component={App} />
-      <Route exact path="/tutorial" component={Tutorial}/>
+      <Route exact path="/tutorial" component={Tutorial} />
       <Route exact path="/tutorialPage/:learn" component={TutorialPage} />
     </Switch>
   </Router>,
