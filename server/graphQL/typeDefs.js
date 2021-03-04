057324bc585c13b80e5da8c0654d0bfa9a06f467
Shana Hoehn <shanahoehn@gmail.com>
2021-03-04 10:29:45 -0800

    test useQuery

--- server/graphQL/typeDefs.js
+++ server/graphQL/typeDefs.js
@@ -4,16 +4,7 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
   # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
-
-  # This "Book" type defines the queryable fields for every book in our data source.
-  type Book {
-    title: String
-    author: Author
-  }
-  type Author {
-    name: String
-    books: [Book]
-  }
+  
   type Mutation {
     addTest(name: String): Test
     updateTest(id: String, name: String): Test
