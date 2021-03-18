a0c5bafc038de312806620c0618fad3a0f6362d4
khuong nguyen <khuongnguyensac@gmail.com>
2021-03-17 20:36:17 -0700

    switching branch

--- __tests__/gql.projects.test.js
+++ __tests__/gql.projects.test.js
@@ -4,12 +4,12 @@ const http = require('http');
 const app = require('../server/server.js');
 
 const { GET_PROJECTS, ADD_LIKE } = require('../mockData');
-const { projectToSave, state } = require('../app/mockData');
+const { projectToSave, state } = require('../mockData');
 
 // tests user signup and login routes
 describe('GraphQL tests', () => {
   let server;
-  let projectId;
+  let projectId = "6052b90a6287fb36e96a2bfe";
 
   beforeAll((done) => {
     server = http.createServer(app);
@@ -30,8 +30,7 @@ describe('GraphQL tests', () => {
         query: GET_PROJECTS,
       })
       .expect(200)
-      .then(res => expect(res.body.data.getAllProjects.length).toBeGreaterThanOrEqual(1)),
-    );
+      .then(res => expect(res.body.data.getAllProjects.length).toBeGreaterThanOrEqual(1)));
 
     it('getAllProjects should return projects that matches the provided userId', () => request(server)
       .post('/graphql')
@@ -40,30 +39,14 @@ describe('GraphQL tests', () => {
         query: GET_PROJECTS,
         variables: {
           userId: '603ac3454625489e492abe16',
-        }
+        },
       })
       .expect(200)
-      .then(res => expect(res.body.data.getAllProjects[0].userId).toBe('603ac3454625489e492abe16')),
-    ); 
+      .then(res => expect(res.body.data.getAllProjects[0].userId).toBe('603ac3454625489e492abe16')));
   });
-  
+
   // GraphQL Mutation
   describe('Testing GraphQL mutation', () => {
-
-    // Create project
-    describe('/POST', () => {
-      it('responds with a status of 200 and json object equal to project sent', () => {
-        return request(server)
-          .post('/saveProject')
-          .set('Accept', 'application/json')
-          .send(projectToSave)
-          .expect(200)
-          .expect('Content-Type', /application\/json/)
-          .then((res) => { projectId = res.body.id; })
-          .then((res) => expect(res.body.name).toBe(projectToSave.name));
-      });
-    });
-
     // Add likes
     it('Increment likes by 1', () => request(server)
       .post('/graphql')
@@ -73,34 +56,32 @@ describe('GraphQL tests', () => {
         variables: {
           projId: projectId,
           likes: 100,
-        }
+        },
       })
       .expect(200)
-      .then(res => expect(res.body.data.likes).toBe(1)),
-    );
-  
-    
-    // Publish project
-  
-    // Make copy
-  
-    // Delete copy
-  
-    // Delete project
-    
-
-    it('getAllProjects should return projects that matches the provided userId', () => request(server)
-      .post('/graphql')
-      .set('Content-Type', 'application/json')
-      .send({
-        query: GET_PROJECTS,
-        variables: {
-          userId: '603ac3454625489e492abe16',
-        }
-      })
-      .expect(200)
-      .then(res => expect(res.body.data.getAllProjects[0].userId).toBe('603ac3454625489e492abe16')),
-    ); 
+      .then(res => expect(res.body.data.likes).toBe(1)));
   });
 
+
+  // Publish project
+
+  // Make copy
+
+  // Delete copy
+
+  // Delete project
+
+
+  // it('getAllProjects should return projects that matches the provided userId', () => request(server)
+  //   .post('/graphql')
+  //   .set('Content-Type', 'application/json')
+  //   .send({
+  //     query: GET_PROJECTS,
+  //     variables: {
+  //       userId: '603ac3454625489e492abe16',
+  //     }
+  //   })
+  //   .expect(200)
+  //   .then(res => expect(res.body.data.getAllProjects[0].userId).toBe('603ac3454625489e492abe16')),
+  // );
 });
