0a13034f718674d045a0e47d6c1c296473412989
Jonathan Calvo <jono.calvo@gmail.com>
2021-04-27 10:12:41 -0700

    Styles CSS Editor Created
    Co-authored-by: buddhajjigae <buddhajjigae@users.noreply.github.com>
    Co-authored-by: xkevinpark <xkevinpark@users.noreply.github.com>
    Co-authored-by: williamdyoon <williamdyoon@users.noreply.github.com>
    Co-authored-by: MadinventorZero <MadinventorZero@users.noreply.github.com>

---
+++ app/src/components/bottom/StylesEditor.tsx
@@ -0,0 +1,99 @@
+import React, { useContext, useState, useRef, useEffect } from 'react';
+import StateContext from '../../context/context';
+import AceEditor from 'react-ace';
+import 'ace-builds/src-noconflict/mode-css';
+import 'ace-builds/src-noconflict/theme-monokai';
+import 'ace-builds/src-noconflict/theme-github';
+import 'ace-builds/src-noconflict/theme-solarized_dark';
+import 'ace-builds/src-noconflict/theme-solarized_light';
+import 'ace-builds/src-noconflict/theme-terminal';
+import 'ace-builds/src-min-noconflict/ext-searchbox';
+import { Component } from '../../interfaces/Interfaces';
+import useResizeObserver from '../../tree/useResizeObserver';
+import { string } from 'prop-types';
+import Fab from '@material-ui/core/Fab';
+import SaveIcon from '@material-ui/icons/Save';
+
+const StylesEditor: React.FC<{
+  theme: string | null;
+  setTheme: any | null;
+}> = ({ theme, setTheme }) => {
+  const wrapper = useRef();
+  const [css, setCss] = useState();
+  // const [state, dispatch] = useContext(StateContext);
+
+  useEffect(() => {
+    loadFile();
+  }, []);
+
+  const loadFile = () => {
+    const myHeaders = new Headers({
+      'Content-Type': 'text/css',
+      Accept: 'text/css',
+    });
+    fetch('/demoRender', {
+      headers: myHeaders,
+    })
+      .then(response => response.text())
+      .then((data) => {
+        setCss(data);
+      });
+  }
+
+  const saveFile = () => {
+    console.log('saveFile: ', css);
+    // const myHeaders = new Headers({
+    //   headers: { 'Content-Type': 'application/json' },
+    // });
+    fetch('/user-styles/save', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ data: css }),
+    })
+      .then(response => response.text())
+      .then((data) => {
+        // setCss(data);
+        //message save!
+      });
+  }
+  const saveCss = (e) => {
+    e.preventDefault();
+    saveFile();
+  }
+
+  const handleChange = (text) => {
+    setCss(text);
+  }
+
+  return (
+    <div
+      className='text-editor'
+      ref={wrapper}
+      style={{
+        height: '40vh',
+        maxWidth: '100%',
+        justifyContent: 'center',
+      }}
+    >
+      <AceEditor
+        mode="css"
+        theme={'solarized_dark'}
+        width="100%"
+        height="100%"
+        onChange={handleChange}
+        value={css}
+        name="Css_div"
+        // readOnly={false}
+        fontSize={16}
+        tabSize={2}
+        enableBasicAutocompletion={true}
+        enableLiveAutocompletion={true}
+      />
+      <Fab className='btn' onClick={saveCss} color="secondary" aria-label="add">
+        <SaveIcon />
+      </Fab>
+    </div>
+  );
+};
+
+export default StylesEditor;
