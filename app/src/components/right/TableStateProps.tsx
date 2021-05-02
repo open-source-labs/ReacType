af432f2ca27aeff065cdbf292f519418f8ca79cc
KP <kp@KPs-MacBook-Pro.local>
2021-05-01 18:30:30 -0700

    Fixed bug that triggers re-rendering. Refactored and modularized re-usable code. Matched styling to main app container.
    Co-authored-by: jonocr <jonocr@users.noreply.github.com>
    Co-authored-by: buddhajjigae <buddhajjigae@users.noreply.github.com>
    Co-authored-by: williamdyoon <williamdyoon@users.noreply.github.com>
    Co-authored-by: MadinventorZero <MadinventorZero@users.noreply.github.com>

--- app/src/components/right/TableStateProps.tsx
+++ app/src/components/right/TableStateProps.tsx
@@ -5,45 +5,14 @@ import {
   GridColumns,
   GridEditRowsModel,
 } from '@material-ui/data-grid';
+import {
+  styled,
+} from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import StateContext from '../../context/context';
-import { PinDropSharp } from '@material-ui/icons';
 
-// const columns: GridColumns = [
-//   {
-//     field: 'id',
-//     headerName: 'ID',
-//     width: 70,
-//     editable: false,
-//   },
-//   {
-//     field: 'key',
-//     headerName: 'Key',
-//     width: 100,
-//     editable: true,
-//   },
-//   {
-//     field: 'value',
-//     headerName: 'Value',
-//     width: 100,
-//     editable: true,
-//   },
-//   {
-//     field: 'type',
-//     headerName: 'Type',
-//     width: 100,
-//     editable: false,
-//   },
-//   {
-//     field: 'delete',
-//     headerName: 'Delete',
-//     width: 100,
-//     editable: false,
-//     renderCell: () => (
-//       <Button>Delete</Button>
-//     ),
-//   },
-// ];
+import ClearIcon from '@material-ui/icons/Clear';
+import { IconButton, SvgIcon  } from '@material-ui/core';
 
 const getColumns = (props) => {
   return [
@@ -56,25 +25,25 @@ const getColumns = (props) => {
     {
       field: 'key',
       headerName: 'Key',
-      width: 100,
+      width: 90,
       editable: true,
     },
     {
       field: 'value',
       headerName: 'Value',
-      width: 100,
+      width: 90,
       editable: true,
     },
     {
       field: 'type',
       headerName: 'Type',
-      width: 100,
+      width: 90,
       editable: false,
     },
     {
       field: 'delete',
-      headerName: 'Delete',
-      width: 100,
+      headerName: 'X',
+      width: 70,
       editable: false,
       renderCell: (params) => {
         const getIdRow = () => {
@@ -83,52 +52,35 @@ const getColumns = (props) => {
           return params.getValue(fields[0]);          
         };
         return ( 
-          <Button onClick={() => {
-            // console.log("*****************1*****************");
-            // console.log('event:', event);
-            // console.log('event.preventDefault():', event.preventDefault())
-            // event.preventDefault();
-            // console.log('event.stopPropagation():', event.stopPropagation)
-            props.deleteHandler(getIdRow()); 
-            // event.stopPropagation();
-          }}>Delete</Button>
+          <Button style={{width:`${3}px`}}
+            onClick={() => {
+              props.deleteHandler(getIdRow());
+            }}>
+            <ClearIcon style={{width:`${15}px`}}/>
+          </Button>
         );
       }
     },
   ];
 };
 
-// This function iterates thru current component's state props array to build rows to display in grid below
-const buildRows = (props) => {
-  const [state, dispatch] = useContext(StateContext);
-  // const [state] = useContext(StateContext);
-  const currentId = state.canvasFocus.componentId;
-  const currentComponent = state.components[currentId - 1];
-  const rows = [];
-  
 
-  for (let i = 0; i < currentComponent.stateProps.length; i += 1) {
-    const newObj = {};
-    newObj.id = currentComponent.stateProps[i].id;
-    newObj.key = currentComponent.stateProps[i].key;
-    newObj.value = currentComponent.stateProps[i].value;
-    newObj.type = currentComponent.stateProps[i].type;
-    rows.push(newObj);
-  }
-  return rows; 
-};
- 
 const TableStateProps = (props) => {
+  const [state, dispatch] = useContext(StateContext);
   const [editRowsModel, setEditRowsModel] = useState < GridEditRowsModel > ({});
   const [gridColumns, setGridColumns] = useState([]);
 
-  const rows = buildRows(props);
+  // get currentComponent by using currently focused component's id
+  const currentId = state.canvasFocus.componentId;
+  const currentComponent = state.components[currentId - 1];
+
+  const rows = currentComponent.stateProps.slice();
   
   // when component gets mounted, sets the gridColumn
   useEffect(() => {
     setGridColumns(getColumns(props));
   }, []);
-
+  
   return (
     <div style={{ height: 400, width: '100%' }}>
       <DataGrid
