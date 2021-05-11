709ee99616c8a74c288a790ddec9fa2cb36b6902
Daryl Foster <dmafduke@gmail.com>
2021-05-11 04:01:17 -0700

    Big fix. Annotations needed some of the keys without random numbers, restricted that back to separators only so Annotations didn't break. Fixed a major bug where components broke on trying to nest inside themselves. Wrote a new function to validate nesting at the component level. Cleaned up DirectChild Render as it was duplicating the renderChildren helperfunction. This also cleaned up the annotations for components when they are dragged and dropped.
    Co-authored-by: jonocr <jonocr@users.noreply.github.com>
    Co-authored-by: buddhajjigae <buddhajjigae@users.noreply.github.com>
    Co-authored-by: xkevinpark <xkevinpark@users.noreply.github.com>
    Co-authored-by: williamdyoon <williamdyoon@users.noreply.github.com>

--- app/src/components/main/DirectChildComponent.tsx
+++ app/src/components/main/DirectChildComponent.tsx
@@ -8,8 +8,8 @@ import { useDrag } from 'react-dnd';
 import { ItemTypes } from '../../constants/ItemTypes';
 import StateContext from '../../context/context';
 import { combineStyles } from '../../helperFunctions/combineStyles';
-import IndirectChild from './IndirectChild';
 import globalDefaultStyle from '../../public/styles/globalDefaultStyles';
+import renderChildren from '../../helperFunctions/renderChildren'
 
 function DirectChildComponent({ childId, type, typeId, style }: ChildElement) {
   const [state, dispatch] = useContext(StateContext);
@@ -64,119 +64,13 @@ function DirectChildComponent({ childId, type, typeId, style }: ChildElement) {
     interactiveStyle
   );
 
-  // helper function to render children of direct child components
-  // all children of direct child components will be indirect components
-  // indirect child components are not interactive with the exception of route links which, when clicked, will change the canvas focus
-  const renderIndirectChildren = (
-    referencedComponent: Component | ChildElement
-  ) => {
-    // iterate through all children of referenced
-    return referencedComponent.children.map(child => {
-      if (child.type === 'Component') {
-        // If indirect child of referenced component is a component, find the top level component referenced by the child
-        const childReferencedComponent: Component = state.components.find(
-          (elem: Component) => elem.id === child.typeId
-        );
-        // combine the styles of the child with the reference component but give higher priority to the child's styles
-
-        const combinedStyle = combineStyles(
-          childReferencedComponent.style,
-          child.style
-        );
-
-        // render an IndirectChild component, and also call renderIndirectChildren recursively to render any of the child Component's children
-        return (
-          <IndirectChild
-            // combine styles of instance and referenced component. instance styles have higher priority
-            key={
-              'indChild' + child.childId.toString() + child.typeId.toString()
-            }
-            style={combinedStyle}
-            placeHolder=""
-            linkId={null}
-            childId={childId}
-          >
-            {renderIndirectChildren(childReferencedComponent)}
-          </IndirectChild>
-        );
-      } else if (child.type === 'HTML Element') {
-        // if indirect child is an HTML element, render an IndirectChild component with no children
-        // if the HTML element has children, then also render its children
-        // get the default style/placeholder value for that type of HTML element
-        // combine the default style of that HTML element and combine in with the custom styles applied to that element
-        const HTMLType: HTMLType = state.HTMLTypes.find(
-          (type: HTMLType) => type.id === child.typeId
-        );
-        const HTMLDefaultStyle = HTMLType.style;
-        const HTMLDefaultPlaceholder = HTMLType.placeHolderShort;
-        const combinedStyle = combineStyles(HTMLDefaultStyle, child.style);
-        return (
-          <React.Fragment
-            key={
-              'indChildFrag' +
-              child.childId.toString() +
-              child.typeId.toString()
-            }
-          >
-            {child.children.length === 0 ? (
-              <IndirectChild
-                style={combinedStyle}
-                placeHolder={HTMLDefaultPlaceholder}
-                linkId={null}
-                childId={childId}
-                key={
-                  'indChildHTML' +
-                  child.childId.toString() +
-                  child.typeId.toString()
-                }
-              >
-                {''}
-              </IndirectChild>
-            ) : (
-              <IndirectChild
-                style={combinedStyle}
-                placeHolder={HTMLDefaultPlaceholder}
-                linkId={null}
-                childId={childId}
-                key={
-                  'indChildNest' +
-                  child.childId.toString() +
-                  child.typeId.toString()
-                }
-              >
-                {renderIndirectChildren(child)}
-              </IndirectChild>
-            )}
-          </React.Fragment>
-        );
-      } else if (child.type === 'Route Link') {
-        // Render a next.js route link
-        // pass the component id of the component referenced in the link as a prop
-        // IndirectChild will render the referenced component name as a clickable link
-        return (
-          <IndirectChild
-            key={
-              'RouteLink' + child.childId.toString() + child.typeId.toString()
-            }
-            style={combinedStyle}
-            placeHolder=""
-            linkId={child.typeId}
-            childId={childId}
-          >
-            {''}
-          </IndirectChild>
-        );
-      }
-    });
-  };
   return (
     <div
       onClick={onClickHandler}
-      // key={'childComp' + childId}
       style={combinedStyle}
       ref={drag}
     >
-      {renderIndirectChildren(referencedComponent)}
+      {renderChildren(referencedComponent.children)}
     </div>
   );
 }
