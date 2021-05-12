e21e5dee2eb81ac4cd9bb19784758141de4efa67
Jonathan Calvo <jono.calvo@gmail.com>
2021-05-12 16:25:27 -0700

    using git for heroku deployment

--- server/models/reactypeModels.js
+++ server/models/reactypeModels.js
@@ -10,7 +10,7 @@
  */
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
-const config = require('../../config');
+// const config = require('../../config');
 
 const SALT_WORK_FACTOR = 14;
 // connect to mongo db
