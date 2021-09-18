b73aaf37a8641c64157f64dd0e781b35897d4f26
pmhua <philiphuahhhh@gmail.com>
2021-09-18 10:48:00 -0700

    finsihed setting up gatsby app connection files for export, Philip, https://github.com/pmhua, Daniel https://github.com/dreille, William https://github.com/wbrittwage, Miles https://github.com/Miles818

--- app/src/utils/createTestSuite.util.ts
+++ app/src/utils/createTestSuite.util.ts
@@ -35,7 +35,7 @@ const createMocksFiles = (path: string, appName: string) => {
 
 const createTestsFiles = (path: string, appName: string) => {
   const filePath:string = `${path}/${appName}/__mocks__/gatspy.js`;
-  let data: `
+  let data = `
   const React = require("react")
   const gatsby = jest.requireActual("gatsby")
 
@@ -71,6 +71,67 @@ const createTestsFiles = (path: string, appName: string) => {
   });
 }
 
+async function createJestConfigFile(path: String, appName: String){
+  const filePath:string = `${path}/${appName}/jest.config.js`;
+  const data:string = `
+  module.exports = {
+    transform: {
+      "^.+\\.tsx?$": "ts-jest",
+      "^.+\\.jsx?$": "<rootDir>/jest-preprocess.js",
+    },
+    testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.([tj]sx?)$",
+    moduleNameMapper: {
+      ".+\\.(css|styl|less|sass|scss)$": "identity-obj-proxy",
+      ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/file-mock.js",
+    },
+    moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+    testPathIgnorePatterns: ["node_modules", ".cache"],
+    transformIgnorePatterns: ["node_modules/(?!(gatsby)/)"],
+    globals: {
+      __PATH_PREFIX__: '',
+    }
+  }
+  `
+  window.api.writeFile(filePath, data, err => {
+    if (err) {
+      console.log('createTestSuite.util createJestConfigFile error:', err.message);
+    } else {
+      console.log('createTestSuit.util createJestConfigFile written successfully');
+    }
+  });
+};
+
+async function createJestPreprocessFile(path: string, appName: string){
+  const filePath: string = `${path}/${appName}/jest-preprocess.js`;
+  const data:string = `
+  const babelOptions = {
+    presets: ["babel-preset-gatsby"],
+  }
+
+  module.exports = require("babel-jest").default.createTransformer(babelOptions)`;
+
+  window.api.writeFile(filePath, data, err => {
+    if (err) {
+      console.log('createTestSuite.util createJestPreprocessFile error:', err.message);
+    } else {
+      console.log('createTestSuit.util createJestPreprocessFile written successfully');
+    }
+  });
+}
+
+async function createComponentTests(path: string, appName: string, components: Component[]) {
+  const filePath: string = `${path}/${appName}/__tests__/test.tsx`;
+  const data:string = '';
+
+  window.api.writeFile(filePath, data, err => {
+    if (err) {
+      console.log('createTestSuite.util createComponentTests error:', err.message);
+    } else {
+      console.log('createTestSuit.util createComponentTests written successfully');
+    }
+  });
+}
+
 async function createTestSuite({
   path,
   appName,
@@ -90,6 +151,9 @@ async function createTestSuite({
   await initFolders(path, appName);
   await createMocksFiles(path, appName);
   await createTestsFiles(path, appName);
+  await createJestConfigFile(path, appName);
+  await createJestPreprocessFile(path, appName);
+  await createComponentTests(path, appName, components);
 }
 
 export default createTestSuite;
