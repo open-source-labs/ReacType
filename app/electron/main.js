c1a919bad4d9bc7b85a5eec9bc3c7914acb80869
Calvin Cao <jtcao430@gmail.com>
2022-02-21 18:14:03 -0800

    fixed bug where error message pops up when user closes out of app through close button rather than file -> exit

--- app/electron/main.js
+++ app/electron/main.js
@@ -124,7 +124,6 @@ async function createWindow() {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    // win = null;
     app.quit();
   });
 
