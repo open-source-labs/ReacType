d7a899eb665e77d4cf21d157ed60f3dd27664e93
Thomas Lukasiewicz <53915559+tlukasiewicz89@users.noreply.github.com>
2022-08-24 22:20:24 -0400

    disable nesting components in html elements

--- app/src/reducers/componentReducer.ts
+++ app/src/reducers/componentReducer.ts
@@ -268,6 +268,7 @@ const reducer = (state: State, action: Action) => {
         if (childTypeExists('Component', parentComponentId, originalComponent))
           return state;
       }
+
       let newName = state.HTMLTypes.reduce((name, el) => {
         if (typeId === el.id) {
           name = type === 'Component' ? componentName : el.tag;
@@ -316,8 +317,14 @@ const reducer = (state: State, action: Action) => {
       // if there is a childId (childId here references the direct parent of the new child) find that child and a new child to its children array
       else {
         directParent = findChild(parentComponent, childId);
-        directParent.children.push(topSeparator);
-        directParent.children.push(newChild);
+        if (directParent.type === "HTML Element" && type === "HTML Element") {
+          directParent.children.push(topSeparator);
+          directParent.children.push(newChild);
+        } else {
+          console.log('sorry cant drag comp into html bruh')
+          return state;
+        }
+        
       }
       const canvasFocus = {
         ...state.canvasFocus,
