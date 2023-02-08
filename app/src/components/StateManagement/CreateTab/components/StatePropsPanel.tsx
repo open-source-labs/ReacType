0a85ed05b0055f641525dbb65fbacc219f00eb9e
wang9hu <xwang.9hu@gmail.com>
2023-02-08 10:37:50 -0800

    bottom panel resize

--- app/src/components/StateManagement/CreateTab/components/StatePropsPanel.tsx
+++ app/src/components/StateManagement/CreateTab/components/StatePropsPanel.tsx
@@ -237,7 +237,7 @@ const StatePropsPanel = ({ isThemeLight, data}): JSX.Element => {
                 Required
             </FormHelperText>
           </FormControl>
-          <br></br>
+          <br />
           <MyButton
           type="submit"
           onClick={submitNewState}
@@ -245,36 +245,37 @@ const StatePropsPanel = ({ isThemeLight, data}): JSX.Element => {
           >
             Save
           </MyButton>
-          <br></br>
+          <br />
 
         </FormControl>
       </div>
-       <br></br>
+       <br />
       <div style={{display: 'flex', overflowX: 'scroll', width: '1700px'}}>
         <div style={{display: 'flex', flexDirection: 'column'}}>
-        <h4  className={isThemeLight ? classes.lightThemeFontColor : classes.darkThemeFontColor}>
-          Current Component State: {state.components[state.canvasFocus.componentId - 1].name}
-        </h4>
-        <TableStateProps rows1={rows1} setRows1={setRows1} canDeleteState = {true} selectHandler={handlerRowSelect} isThemeLight={isThemeLight} data={data}/>
+          <h4  className={isThemeLight ? classes.lightThemeFontColor : classes.darkThemeFontColor}>
+            Current Component State: {state.components[state.canvasFocus.componentId - 1].name}
+          </h4>
+          <TableStateProps rows1={rows1} setRows1={setRows1} canDeleteState = {true} selectHandler={handlerRowSelect} isThemeLight={isThemeLight} data={data}/>
         </div>
     
         <div style={{display: 'flex', flexDirection: 'column'}}>
-        <h4 className={isThemeLight ? classes.lightThemeFontColor : classes.darkThemeFontColor}>
-          Available Props from Parent: {parentName ? parentName : 'No Parents'}
-        </h4>
-        <TableParentProps parentPassedInProps = {parentPassedInProps} parentComponent ={parentComponent} parentProps={parentProps} canDeleteState = {true} selectHandler={handlerRowSelect} isThemeLight={isThemeLight} data={data}/>
+          <h4 className={isThemeLight ? classes.lightThemeFontColor : classes.darkThemeFontColor}>
+            Available Props from Parent: {parentName ? parentName : 'No Parents'}
+          </h4>
+          <TableParentProps parentPassedInProps = {parentPassedInProps} parentComponent ={parentComponent} parentProps={parentProps} canDeleteState = {true} selectHandler={handlerRowSelect} isThemeLight={isThemeLight} data={data}/>
         </div>
+
         <div style={{display: 'flex', flexDirection: 'column', width: `${40}px`, color: 'black', justifyContent: 'center'}}>
-        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-arrow-right-circle-fill" viewBox="0 0 16 16">
-  <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
-</svg>
-      </div>
+          <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-arrow-right-circle-fill" viewBox="0 0 16 16">
+            <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
+          </svg>
+        </div>
 
         <div style={{display: 'flex', flexDirection: 'column'}}>
-        <h4  className={isThemeLight ? classes.lightThemeFontColor : classes.darkThemeFontColor}>
-        Passed in Props from Parent: {parentName ? parentName : 'No Parents'}
-        </h4>
-        <TablePassedInProps canDeleteState = {true} selectHandler={handlerRowSelect} isThemeLight={isThemeLight} data={data}/>
+          <h4  className={isThemeLight ? classes.lightThemeFontColor : classes.darkThemeFontColor}>
+            Passed in Props from Parent: {parentName ? parentName : 'No Parents'}
+          </h4>
+          <TablePassedInProps canDeleteState = {true} selectHandler={handlerRowSelect} isThemeLight={isThemeLight} data={data}/>
         </div>
       </div>
     </div>
--- app/src/components/bottom/BottomPanel.tsx
+++ app/src/components/bottom/BottomPanel.tsx
@@ -1,19 +1,60 @@
-import React from 'react';
+import React, { RefObject, useEffect, useRef } from 'react';
 import BottomTabs from './BottomTabs';
-import { Resizable } from 're-resizable';
+// import { Resizable } from 're-resizable';
 
 const BottomPanel = (props): JSX.Element => {
+  let y: number = 0;
+  let h: number = 0;
+
+  const node = useRef() as React.MutableRefObject<HTMLDivElement>;
+  
+  const mouseDownHandler = (e): void => {
+    y = e.clientY;
+
+    const styles = window.getComputedStyle(node.current);
+    h = parseInt(styles.height, 10);
+
+    document.addEventListener('mousemove', mouseMoveHandler);
+    document.addEventListener('mouseup', mouseUpHandler);
+  } 
+
+  const mouseMoveHandler = function (e: MouseEvent): void {
+    // How far the mouse has been moved
+    const dy = y - e.clientY;
+
+    // Adjust the dimension of element
+    node.current.style.height = `${h + dy}px`;
+  };
+
+  const mouseUpHandler = function () {
+      // Remove the handlers of `mousemove` and `mouseup`
+      document.removeEventListener('mousemove', mouseMoveHandler);
+      document.removeEventListener('mouseup', mouseUpHandler);
+  };
+
+  useEffect(() => {
+    node.current.style.height = '50vh';
+  },[]);
+
   return (
-    <Resizable
-      enable={{
-        top: true
-      }}
-      minHeight={'50%'}
-    >
-      <div className="bottom-panel" >
-        <BottomTabs isThemeLight={props.isThemeLight}/>
-      </div>
-    </Resizable>
+    // decide not to use re-resizable library
+    // <Resizable
+    //   defaultSize={{ width: 'auto', height: '50%' }}
+    //   enable={{
+    //     top: true
+    //   }}
+    //   // minHeight={'50%'}
+    // >
+    //   <div className="bottom-panel" >
+    //     <BottomTabs isThemeLight={props.isThemeLight}/>
+    //   </div>
+    // </Resizable>
+    
+    // add 'tabIndex' to make the resize bar focusable
+    <div className="bottom-panel" id='resize' ref={node}>
+      <div id='resize-drag' onMouseDown={mouseDownHandler} tabIndex = {0} >......</div> 
+      <BottomTabs isThemeLight={props.isThemeLight}/>
+    </div>
   );
 };
 
