c3fb56f032dc572ecac5d9743f5bea13cf9b09e6
Mike Dunnmon <99291081+mdunnmon@users.noreply.github.com>
2023-03-14 10:01:39 -0700

    commented out signup theme

--- app/src/components/login/SignUp.tsx
+++ app/src/components/login/SignUp.tsx
@@ -46,7 +46,7 @@ function Copyright() {
 
 const useStyles = makeStyles(theme => ({
   paper: {
-    marginTop: theme.spacing(8),
+    // marginTop: theme.spacing(8),
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center'
@@ -57,10 +57,10 @@ const useStyles = makeStyles(theme => ({
   },
   form: {
     width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(3)
+    // marginTop: theme.spacing(3)
   },
   submit: {
-    margin: theme.spacing(3, 0, 2)
+    // margin: theme.spacing(3, 0, 2)
   },
   root: {
     '& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline': {
