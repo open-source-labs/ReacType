ea96a03c8d4c027c7a94a38606b74c19e36c3874
Rachel Kucharski <rachel.kucharski@codesmith.io>
2023-04-24 18:00:03 -0700

    Revealed Export Button to Guests

--- app/src/helperFunctions/cssRefresh.tsx
+++ app/src/helperFunctions/cssRefresh.tsx
@@ -1,13 +1,15 @@
 // Removes old link to css and creates a new stylesheet link on demo render
 const cssRefresher = () => {
   const oldStylesheet = document.getElementById('Render Stylesheet')
+  console.log(oldStylesheet);
   if (oldStylesheet !== null) oldStylesheet.remove();
   const rando = Math.random() * 100000;
   const newStylesheet = document.createElement("LINK");
   newStylesheet.setAttribute("rel", "stylesheet")
   newStylesheet.setAttribute("type", "text/css");
-  newStylesheet.setAttribute("href", `https://reactype-caret.herokuapp.com/demoRender?${rando}`);
+  newStylesheet.setAttribute("href", `https://reactype-caret.herokuapp.com/#/demoRender?${rando}`);
   newStylesheet.setAttribute("id", 'Render Stylesheet');
   document.getElementById('renderFocus').append(newStylesheet);
+  console.log(newStylesheet)
 }
 export default cssRefresher;
