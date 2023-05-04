eaad03742d17b27139d804e6480ff5caf5939573
atvanek <99850013+atvanek@users.noreply.github.com>
2023-05-04 19:10:52 -0400

    Got display tab to render tree

--- app/src/components/ContextAPIManager/DisplayTab/DisplayContainer.tsx
+++ app/src/components/ContextAPIManager/DisplayTab/DisplayContainer.tsx
@@ -6,21 +6,39 @@ import { RootState } from '../../../redux/store';
 
 const DisplayContainer = () => {
   const allContext = useSelector((store: RootState) => store.contextSlice);
+  // const { allContext } = store.getState().contextSlice;
   const [contextData, setContextData] = useState([]);
 
   //build data for Google charts, tree rendering
   useEffect(() => {
-    if (allContext.length) {
-      const formattedData = allContext
-        .map((el) => {
-          return el.components.map((component) => {
-            return [`App - ${el.name} - ${component}`];
-          });
-        })
-        .flat();
-      setContextData([['Phrases'], ...formattedData]);
-    }
+    console.log('this is context data', allContext);
+    transformData(allContext);
   }, []);
+
+  [
+    {
+      name: 'Test',
+      values: [
+        {
+          key: 'sdafasdasdfsd',
+          value: 'fasdfas'
+        }
+      ],
+      components: ['Dfasdfa']
+    }
+  ];
+
+  const transformData = (contexts) => {
+    const formattedData = contexts.allContext
+      .map((obj) => {
+        return obj.components.map((component) => {
+          return [`App ${obj.name} ${component}`];
+        });
+      })
+      .flat();
+    console.log(formattedData);
+    setContextData([['Phrases'], ...formattedData]);
+  };
 
   const options = {
     wordtree: {
@@ -31,15 +49,13 @@ const DisplayContainer = () => {
   return (
     <Grid container display="flex" justifyContent="center">
       <Grid item>
-        {contextData.length > 0 && (
-          <Chart
-            chartType="WordTree"
-            width="100%"
-            height="450px"
-            data={contextData}
-            options={options}
-          />
-        )}
+        <Chart
+          chartType="WordTree"
+          width="100%"
+          height="450px"
+          data={contextData}
+          options={options}
+        />
       </Grid>
     </Grid>
   );
