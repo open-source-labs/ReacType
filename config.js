5b821d44577a7ebfe55b2c76c6898ea700f2dbd9
@MatteoDiter <115022965+MatteoDiter@users.noreply.github.com>
2023-05-17 11:20:01 -0400

    server fixed Co-authored-by: Sophia Bui <sophia-bui@users.noreply.github.com> Co-authored-by: rachelk585 <rachelk585@users.noreply.github.com> Co-authored-by: Adam Vanek <atvanek@users.noreply.github.com>

--- config.js
+++ config.js
@@ -1,13 +1,11 @@
 const isProduction = process.env.NODE_ENV === 'production';
-
 const config = {
   DEV_PORT: 5656,
   API_BASE_URL: isProduction
-    ? 'http://reactypev15-env.eba-mbvivk7k.us-east-1.elasticbeanstalk.com'
-    : 'http://localhost:8080',
+    ? 'https://app.reactype.dev'
+    : 'http://localhost:5656',
   API_BASE_URL2: isProduction
-    ? 'http://reactypev15-env.eba-mbvivk7k.us-east-1.elasticbeanstalk.com'
+    ? 'https://app.reactype.dev'
     : 'http://localhost:8080',
 };
-
 module.exports = config;
