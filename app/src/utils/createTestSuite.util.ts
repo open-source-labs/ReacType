50ed805495534d871014498b50f4c6ca5b67a614
Sonya Hu <sonyahu15@gmail.com>
2023-11-10 15:22:07 -0500

    trying to fix uuid

--- app/src/utils/createTestSuite.util.ts
+++ app/src/utils/createTestSuite.util.ts
@@ -73,7 +73,7 @@ async function createJestConfigFile(path: String, appName: String) {
     moduleNameMapper: {
       ".+\\.(css|styl|less|sass|scss)$": "identity-obj-proxy",
       ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/file-mock.js",
-      "uuid": require.resolve('uuid'),
+      "^uuid$": "uuid",
     },
     moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
     testPathIgnorePatterns: ["node_modules", ".cache"],
--- app/src/utils/createTestSuiteNext.util.ts
+++ app/src/utils/createTestSuiteNext.util.ts
@@ -42,7 +42,7 @@ module.exports = {
   moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/file-mock.js",
     "\\.(css|less|scss)$": "identity-obj-proxy",
-    "uuid": require.resolve('uuid'),
+    "^uuid$": "uuid",
   }
 }
 `;
