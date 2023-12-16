eff399deb9808140ef21ef17a15044ea7e55058c
Laura <laforden721@gmail.com>
2023-12-16 06:36:45 -0800

    fixed tracking for x'y on mouse tracker

--- app/src/components/main/MouseMovement.tsx
+++ app/src/components/main/MouseMovement.tsx
@@ -3,6 +3,18 @@ import { MouseState, MouseProps } from '../../interfaces/Interfaces';
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 
+const width = '200px';
+
+const AppStyle: React.CSSProperties = {
+  width,
+  height: width,
+  backgroundColor: 'lightcoral'
+};
+
+const textStyle: React.CSSProperties = {
+  color: '#a5ead6'
+};
+
 class MouseMovement extends Component<MouseProps, MouseState> {
   state = {
     clientX: 0,
@@ -10,8 +22,8 @@ class MouseMovement extends Component<MouseProps, MouseState> {
   };
   setclient = (clientX, clientY) => {
     this.setState({ clientX, clientY });
-    console.log('clientX', clientX);
-    console.log('clientY', clientY);
+    // console.log('clientX', clientX);
+    // console.log('clientY', clientY);
   };
 
   render() {
@@ -20,10 +32,11 @@ class MouseMovement extends Component<MouseProps, MouseState> {
     } = this;
     return [
       <div
+        style={AppStyle}
         onMouseMove={({ clientX, clientY }) => this.setclient(clientX, clientY)}
       ></div>,
-      <p>clientX: {clientX} </p>,
-      <p>clientY: {clientY} </p>
+      <p style={textStyle}>clientX: {clientX} </p>,
+      <p style={textStyle}>clientY: {clientY} </p>
     ];
   }
 }
