1cacfebd2870e96c7122425ede62f786c48e6c5b
Sean <kingsora999@gmail.com>
2024-01-08 15:29:36 -0800

    Websocket emitter for addroute

--- app/src/components/main/AddRoute.tsx
+++ app/src/components/main/AddRoute.tsx
@@ -3,12 +3,10 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { addChild } from '../../redux/reducers/slice/appStateSlice';
 import { RootState } from '../../redux/store';
-import { PanoramaSharp } from '@mui/icons-material';
 import { emitEvent } from '../../helperFunctions/socket';
 
-const roomCode = useSelector((store: RootState) => store.roomSlice.roomCode);
-
 function AddRoute({ id }: AddRoutes): JSX.Element {
+  const roomCode = useSelector((store: RootState) => store.roomSlice.roomCode);
   const dispatch = useDispatch();
   const contextParam = useSelector((store: RootState) => store.contextSlice);
   const handleClick = (id: number): void => {
@@ -27,8 +25,11 @@ function AddRoute({ id }: AddRoutes): JSX.Element {
         childId: id,
         contextParam: contextParam
       });
+
+      console.log('emit addChildAction event is triggered in AddRoute!');
     }
   };
+
   return (
     <div style={{ padding: '1px', float: 'right' }}>
       <button id={'routeBtn' + id} onClick={() => handleClick(id)}>
