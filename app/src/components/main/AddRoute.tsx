7bc5cf8e335e190acd7d7c819870a36eaf286d45
Sean <kingsora999@gmail.com>
2024-01-08 13:57:08 -0800

    Electron to typescript

--- app/src/components/main/AddRoute.tsx
+++ app/src/components/main/AddRoute.tsx
@@ -3,6 +3,10 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { addChild } from '../../redux/reducers/slice/appStateSlice';
 import { RootState } from '../../redux/store';
+import { PanoramaSharp } from '@mui/icons-material';
+import { emitEvent } from '../../helperFunctions/socket';
+
+const roomCode = useSelector((store: RootState) => store.roomSlice.roomCode);
 
 function AddRoute({ id }: AddRoutes): JSX.Element {
   const dispatch = useDispatch();
@@ -16,8 +20,15 @@ function AddRoute({ id }: AddRoutes): JSX.Element {
         contextParam: contextParam
       })
     );
+    if (roomCode) {
+      emitEvent('addChildAction', roomCode, {
+        type: 'HTML Element',
+        typeId: -1,
+        childId: id,
+        contextParam: contextParam
+      });
+    }
   };
-
   return (
     <div style={{ padding: '1px', float: 'right' }}>
       <button id={'routeBtn' + id} onClick={() => handleClick(id)}>
