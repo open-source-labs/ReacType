3aaa3b0b978205393c3a5058c8796253f5588e6e
John Wage <john.m.wage@gmail.com>
2024-02-06 10:43:27 -0500

    prop drilled bottomShow piece of state from MainContainer parent component to BottomPanel child component to change expand and collapse material ui icons. And changed onMouseOver event handler to onClick event handler on BottomTabs component. Install more Material UI dependencies also

--- app/src/components/bottom/BottomTabs.tsx
+++ app/src/components/bottom/BottomTabs.tsx
@@ -57,7 +57,7 @@ const BottomTabs = (props): JSX.Element => {
         zIndex: 1,
         borderTop: '2px solid grey'
       }}
-      onMouseOver={() => {
+      onClick={() => {
         props.setBottomShow(true);
       }}
     >
