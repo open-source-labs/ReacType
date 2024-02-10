f6213f9787f0f996585dc6a69349bee39b52bf1a
cyburns <72774499+cyburns@users.noreply.github.com>
2024-02-10 10:51:03 -0500

    with comments added back

--- app/src/components/left/RoomsContainer.tsx
+++ app/src/components/left/RoomsContainer.tsx
@@ -5,7 +5,6 @@ import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
 import Button from '@mui/material/Button';
-import React from 'react';
 import { RootState } from '../../redux/store';
 import TextField from '@mui/material/TextField';
 import { BottomPanelObj } from '../../interfaces/Interfaces';
@@ -53,7 +52,7 @@ import {
   DeleteContextPayload,
   addComponentToContext
 } from '../../../src/redux/reducers/slice/contextReducer';
-import { initSocketConnection } from '../socketUtils/socket';
+// import { initSocketConnection } from '../socketUtils/socket';
 
 const RoomsContainer = () => {
   const dispatch = useDispatch();
@@ -68,166 +67,198 @@ const RoomsContainer = () => {
     (store: RootState) => store.roomSlice.userJoined
   );
 
-  // const initSocketConnection = (roomCode: string) => {
-  //   initializeSocket();
-  //   const socket = getSocket();
-  //   if (socket) {
-  //     socket.on('connect', () => {
-  //       socket.emit('joining', userName, roomCode);
-  //     });
+  const initSocketConnection = (roomCode: string) => {
+    // helper function to create socket connection
+    initializeSocket();
+    // assign socket to result of helper function to return socket created
+    const socket = getSocket();
+    // if socket was created correctly and exists
+    if (socket) {
+      //run everytime when a client connects to server
+      socket.on('connect', () => {
+        socket.emit('joining', userName, roomCode);
+        // console.log(`${userName} Joined room ${roomCode} from RoomsContainer`);
+      });
 
-  //     socket.on('requesting state from host', (callback) => {
-  //       const newState = store.getState();
-  //       callback(newState);
-  //     });
+      //If you are the host: send current state to server when a new user joins
+      socket.on('requesting state from host', (callback) => {
+        const newState = store.getState(); //pull the current state
+        callback(newState); //send it to backend server
+      });
 
-  //     socket.on('server emitting state from host', (state, callback) => {
-  //       store.dispatch(allCooperativeState(state.appState));
-  //       store.dispatch(codePreviewCooperative(state.codePreviewCooperative));
-  //       store.dispatch(cooperativeStyle(state.styleSlice));
-  //       callback({ status: 'confirmed' });
-  //     });
+      //If you are the new user: receive the state from the host
+      socket.on('server emitting state from host', (state, callback) => {
+        //dispatching new state to change user current state
+        // console.log('state received by new join:', state);
+        store.dispatch(allCooperativeState(state.appState));
+        store.dispatch(codePreviewCooperative(state.codePreviewCooperative));
+        store.dispatch(cooperativeStyle(state.styleSlice));
+        callback({ status: 'confirmed' });
+      });
 
-  //     socket.on('updateUserList', (newUserList) => {
-  //       dispatch(setUserList(newUserList));
-  //     });
+      // update user list when there's a change: new join or leave the room
+      socket.on('updateUserList', (newUserList) => {
+        //console.log('user list received from server');
+        dispatch(setUserList(newUserList));
+      });
 
-  //     socket.on('child data from server', (childData: object) => {
-  //       store.dispatch(addChild(childData));
-  //     });
+      // dispatch add child to local state when element has been added by another user
+      socket.on('child data from server', (childData: object) => {
+        // console.log('child data received by users', childData);
+        store.dispatch(addChild(childData));
+      });
 
-  //     socket.on('focus data from server', (focusData: object) => {
-  //       store.dispatch(changeFocus(focusData));
-  //     });
+      // dispatch changeFocus to local state when another user has changed focus by selecting element on canvas
+      socket.on('focus data from server', (focusData: object) => {
+        // console.log('focus data received from server', focusData);
+        store.dispatch(changeFocus(focusData));
+      });
 
-  //     socket.on('delete data from server', (deleteData: object) => {
-  //       store.dispatch(deleteChild(deleteData));
-  //     });
+      // dispatch deleteChild to local state when another user has deleted an element
+      socket.on('delete data from server', (deleteData: object) => {
+        // console.log('delete data received from server', deleteData);
+        store.dispatch(deleteChild(deleteData));
+      });
 
-  //     socket.on(
-  //       'delete element data from server',
-  //       (deleteElementData: object) => {
-  //         store.dispatch(deleteElement(deleteElementData));
-  //       }
-  //     );
+      // dispatch delete element to local state when another user has deleted an element
+      socket.on(
+        'delete element data from server',
+        (deleteElementData: object) => {
+          // console.log('delete element data received from server', deleteElementData);
+          store.dispatch(deleteElement(deleteElementData));
+        }
+      );
 
-  //     // dispatch clear canvas action to local state when the host of the room has clear canvas
-  //     socket.on('clear canvas from server', () => {
-  //       store.dispatch(resetAllState());
-  //     });
+      // dispatch all updates to local state when another user has saved from Bottom Panel
+      socket.on('update data from server', (updateData: BottomPanelObj) => {
+        // console.log('update data received from server', updateData);
+        store.dispatch(
+          updateStateUsed({
+            stateUsedObj: updateData.stateUsedObj,
+            contextParam: updateData.contextParam
+          })
+        );
+        store.dispatch(
+          updateUseContext({
+            useContextObj: updateData.useContextObj,
+            contextParam: updateData.contextParam
+          })
+        );
+        store.dispatch(
+          updateCss({
+            style: updateData.style,
+            contextParam: updateData.contextParam
+          })
+        );
+        store.dispatch(
+          updateAttributes({
+            attributes: updateData.attributes,
+            contextParam: updateData.contextParam
+          })
+        );
+        store.dispatch(
+          updateEvents({
+            events: updateData.events,
+            contextParam: updateData.contextParam
+          })
+        );
+      });
 
-  //     // dispatch all updates to local state when another user has saved from Bottom Panel
-  //     socket.on('update data from server', (updateData: BottomPanelObj) => {
-  //       store.dispatch(
-  //         updateStateUsed({
-  //           stateUsedObj: updateData.stateUsedObj,
-  //           contextParam: updateData.contextParam
-  //         })
-  //       );
-  //       store.dispatch(
-  //         updateUseContext({
-  //           useContextObj: updateData.useContextObj,
-  //           contextParam: updateData.contextParam
-  //         })
-  //       );
-  //       store.dispatch(
-  //         updateCss({
-  //           style: updateData.style,
-  //           contextParam: updateData.contextParam
-  //         })
-  //       );
-  //       store.dispatch(
-  //         updateAttributes({
-  //           attributes: updateData.attributes,
-  //           contextParam: updateData.contextParam
-  //         })
-  //       );
-  //       store.dispatch(
-  //         updateEvents({
-  //           events: updateData.events,
-  //           contextParam: updateData.contextParam
-  //         })
-  //       );
-  //     });
+      // dispatch update style in local state when CSS panel is updated on their side
+      socket.on('update css data from server', (cssData: object) => {
+        // console.log('CSS data received from server', cssData);
+        store.dispatch(updateStylesheet(cssData));
+      });
 
-  //     socket.on('update css data from server', (cssData: object) => {
-  //       store.dispatch(updateStylesheet(cssData));
-  //     });
+      // dispatch new item position in local state when item position is changed by another user
+      socket.on(
+        'item position data from server',
+        (itemPositionData: object) => {
+          // console.log(
+          //   'item position data received from server',
+          //   itemPositionData
+          // );
+          store.dispatch(changePosition(itemPositionData));
+        }
+      );
 
-  //     socket.on(
-  //       'item position data from server',
-  //       (itemPositionData: object) => {
-  //         store.dispatch(changePosition(itemPositionData));
-  //       }
-  //     );
+      // dispatch addComponent to local state when new component is created by another user
+      socket.on('new component data from server', (newComponent: object) => {
+        store.dispatch(addComponent(newComponent));
+      });
 
-  //     socket.on('new component data from server', (newComponent: object) => {
-  //       store.dispatch(addComponent(newComponent));
-  //     });
+      // dispatch addElement to local state when new element is created by another user
+      socket.on('new element data from server', (newElement: object) => {
+        store.dispatch(addElement(newElement));
+      });
 
-  //     socket.on('new element data from server', (newElement: object) => {
-  //       store.dispatch(addElement(newElement));
-  //     });
+      // dispatch addState to local state when component state has been changed by another user
+      socket.on(
+        'new component state data from server',
+        (componentState: object) => {
+          store.dispatch(addState(componentState));
+        }
+      );
 
-  //     socket.on(
-  //       'new component state data from server',
-  //       (componentState: object) => {
-  //         store.dispatch(addState(componentState));
-  //       }
-  //     );
+      // dispatch deleteState to local state when component state has been deleted by another user
+      socket.on(
+        'delete component state data from server',
+        (componentStateDelete: object) => {
+          store.dispatch(deleteState(componentStateDelete));
+        }
+      );
 
-  //     socket.on(
-  //       'delete component state data from server',
-  //       (componentStateDelete: object) => {
-  //         store.dispatch(deleteState(componentStateDelete));
-  //       }
-  //     );
+      // dispatch addPassedInProps to local state when p.I.P have been added by another user
+      socket.on(
+        'new PassedInProps data from server',
+        (passedInProps: object) => {
+          store.dispatch(addPassedInProps(passedInProps));
+        }
+      );
 
-  //     socket.on(
-  //       'new PassedInProps data from server',
-  //       (passedInProps: object) => {
-  //         store.dispatch(addPassedInProps(passedInProps));
-  //       }
-  //     );
+      // dispatch deletePassedInProps to local state when p.I.P have been deleted by another user
+      socket.on(
+        'PassedInProps delete data from server',
+        (passedInProps: object) => {
+          store.dispatch(deletePassedInProps(passedInProps));
+        }
+      );
 
-  //     socket.on(
-  //       'PassedInProps delete data from server',
-  //       (passedInProps: object) => {
-  //         store.dispatch(deletePassedInProps(passedInProps));
-  //       }
-  //     );
+      // dispatch addContext to local state when context has been changed by another user
+      socket.on('new context from server', (context: AddContextPayload) => {
+        store.dispatch(addContext(context));
+      });
 
-  //     socket.on('new context from server', (context: AddContextPayload) => {
-  //       store.dispatch(addContext(context));
-  //     });
+      // dispatch addContextValues to local state when context values are added by another user
+      socket.on(
+        'new context value from server',
+        (contextVal: AddContextValuesPayload) => {
+          store.dispatch(addContextValues(contextVal));
+        }
+      );
 
-  //     socket.on(
-  //       'new context value from server',
-  //       (contextVal: AddContextValuesPayload) => {
-  //         store.dispatch(addContextValues(contextVal));
-  //       }
-  //     );
+      // dispatch deleteContext to local state when context is deleted by another user
+      socket.on(
+        'delete context data from server',
+        (context: DeleteContextPayload) => {
+          store.dispatch(deleteContext(context));
+        }
+      );
 
-  //     socket.on(
-  //       'delete context data from server',
-  //       (context: DeleteContextPayload) => {
-  //         store.dispatch(deleteContext(context));
-  //       }
-  //     );
-
-  //     socket.on('assign context data from server', (data) => {
-  //       store.dispatch(
-  //         addComponentToContext({
-  //           context: data.context,
-  //           component: data.component
-  //         })
-  //       );
-  //       store.dispatch(
-  //         deleteElement({ id: 'FAKE_ID', contextParam: data.contextParam })
-  //       );
-  //     });
-  //   }
-  // };
+      // dispatch addComponentToContext to local state when context is assigned to component by another user
+      socket.on('assign context data from server', (data) => {
+        store.dispatch(
+          addComponentToContext({
+            context: data.context,
+            component: data.component
+          })
+        );
+        store.dispatch(
+          deleteElement({ id: 'FAKE_ID', contextParam: data.contextParam })
+        );
+      });
+    }
+  };
 
   const handleUserEnteredRoom = (roomCode) => {
     initSocketConnection(roomCode);
