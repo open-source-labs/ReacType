38e01849bee45970d73f6c87339c38912bf4f6b2
noahFavelo <nnfavelo@gmail.com>
2024-12-05 18:22:01 -0500

    remove thier link to thier heroku app.

--- config.js
+++ config.js
@@ -2,11 +2,12 @@ const isProduction = process.env.NODE_ENV === 'production';
 const config = {
   DEV_PORT: 5656,
   API_BASE_URL: isProduction
-    ? 'https://app.reactype.dev'
+    ? 'https://localhost:5656'
     : 'http://localhost:5656',
+  // Note, here is where you need to change if you are hosting on a differnt place.
   // : 'http://localhost:8080',
   API_BASE_URL2: isProduction
-    ? 'https://app.reactype.dev'
-    : 'http://localhost:8080',
+    ? 'https://localhost:5656'
+    : 'http://localhost:8080'
 };
 module.exports = config;
