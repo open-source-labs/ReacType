60a2b83805a07e464ae7c1f4b4dce5bead71c6f2
noahFavelo <nnfavelo@gmail.com>
2024-12-05 18:27:33 -0500

    we also cant provide a secure connection yet

--- config.js
+++ config.js
@@ -2,12 +2,12 @@ const isProduction = process.env.NODE_ENV === 'production';
 const config = {
   DEV_PORT: 5656,
   API_BASE_URL: isProduction
-    ? 'https://localhost:5656'
+    ? 'http://localhost:5656'
     : 'http://localhost:5656',
   // Note, here is where you need to change if you are hosting on a differnt place.
   // : 'http://localhost:8080',
   API_BASE_URL2: isProduction
-    ? 'https://localhost:5656'
+    ? 'http://localhost:5656'
     : 'http://localhost:8080'
 };
 module.exports = config;
